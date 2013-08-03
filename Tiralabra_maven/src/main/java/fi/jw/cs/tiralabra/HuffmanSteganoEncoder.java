39c1394862e0687eaba3c493705e7da10ac992f5
Jan Wikholm <jw@jw.fi>
2013-08-02 18:20:47 -0700

    Whole pipeline manual test

--- Tiralabra_maven/src/main/java/fi/jw/cs/tiralabra/HuffmanSteganoEncoder.java
+++ Tiralabra_maven/src/main/java/fi/jw/cs/tiralabra/HuffmanSteganoEncoder.java
@@ -9,41 +9,51 @@ import java.io.IOException;
 
 public class HuffmanSteganoEncoder {
     public static void main(String... args) {
-//        String message = "";
-//        for (String s : args) {
-//            message += s + " ";
-//        }
-//        message = message.trim();
-//        System.out.println("Message \t" + message);
-//        Huffman encoder = new Huffman(message);
-//        encoder.encode();
-//
-//        System.out.print("Encoded \t");
-//        System.out.println(encoder.getEncodedMessage());
-//
-//        Huffman decoder = new Huffman();
-//        decoder.setMap(encoder.getMap());
-//        decoder.setEncodedMessage(encoder.getEncodedMessage());
-//        decoder.decode();
-//        System.out.print("Decoded \t");
-//        System.out.println(decoder.getMessage());
-
         try {
-            String message = "111000111";
-            System.out.println("Open source file " + args[0]);
-            System.out.println("Encode: \t" + message);
-            Steganographer s = new Steganographer(args[0], message);
+
+            String message = "";
+            String src = args[0];
+            String dest = args[1];
+
+            for (int i = 2; i < args.length; i++) {
+                message += args[i] + " ";
+            }
+
+            message = message.trim();
+            System.out.println("Message \t" + message);
+            Huffman encoder = new Huffman(message);
+            encoder.encode();
+            String encodedMessage = encoder.getEncodedMessage();
+
+            System.out.print("Encoded \t");
+            System.out.println(encodedMessage);
+
+
+            System.out.println("Open source file " + src);
+            System.out.println("Encode: \t" + encodedMessage);
+            Steganographer s = new Steganographer(src, encodedMessage);
             s.encode();
-            s.saveFile(args[1]);
+            s.saveFile(dest);
             s = null;
-            System.out.println("File saved to " + args[1]);
+            System.out.println("File saved to " + dest);
+            System.out.println("Opening new Steganographer for " + dest);
             Steganographer dec = new Steganographer(args[1]);
-            System.out.println("Decoding bits from " + args[1]);
             dec.decodeBits();
-            System.out.println("Decode: \t" + dec.getMessage());
+            String stegDecoded = dec.getMessage();
+            System.out.println("Decoded: \t" + stegDecoded);
+            System.out.println("Handing it over to Huffman");
+
+            Huffman decoder = new Huffman();
+            decoder.setMap(encoder.getMap());
+            decoder.setEncodedMessage(stegDecoded);
+            decoder.decode();
+            System.out.print("Decoded \t");
+            System.out.println(decoder.getMessage());
         } catch (IOException ioe) {
             System.out.println("Oh poop.");
             ioe.printStackTrace();
         }
+
+
     }
 }
