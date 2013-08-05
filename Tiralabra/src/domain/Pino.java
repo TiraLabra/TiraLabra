b6835bd286f27112c5d0a17a14c786af39e0e991
Jukka Pitkanen <jukkapit@dhp-18.cs.helsinki.fi>
2013-08-05 16:02:36 +0300

    paivitysta

--- Tiralabra/src/domain/Pino.java
+++ Tiralabra/src/domain/Pino.java
@@ -1,8 +1,51 @@
 package domain;
 
+
 /**
  *
  * @author jukkapit
  */
-public class Pino {
+public class Pino {    
+    private int top;
+    private Object[] table;
+/**
+ * Konstruktori
+ * @param pinonKoko 
+ */
+    public Pino(int pinonKoko) {
+        top = -1;
+        table = new Object[pinonKoko];
+    }
+    /**
+     * 
+     * @param lisattava 
+     */
+    public void push(Object lisattava){
+        top++;
+        table[top] = lisattava;        
+    }
+    /**
+     * 
+     * @return 
+     */
+    public Object pop(){
+        Object poistettava = table[top];
+        top--;
+        return poistettava;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public boolean empty(){
+        return top==-1;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public Object peek(){
+        return table[top];
+    }
+    
 }
