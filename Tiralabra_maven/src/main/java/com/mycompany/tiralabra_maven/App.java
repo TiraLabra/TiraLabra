ca42652836ba3cefe5d9f88388c92dd0926ccd36
Pia Kumpulainen <pia.kumpulainen@cs.helsinki.fi>
2013-08-07 21:13:26 +0300

    Luokkien toteutus aloitettu

--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
@@ -1,13 +1,83 @@
 package com.mycompany.tiralabra_maven;
 
 /**
- * Pia Kumpulainen 
+ * 
+ * @author Pia Kumpulainen 
  * Lyhyin reitti
+ */
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author piakumpu
  */
 public class App 
 {
+    /**
+     *
+     * @param args
+     */
+    private LinkedList<String> kulkuneuvo;
+    /**
+     *
+     */
+    public static Scanner lukija = new Scanner(System.in);
+    
+    /**
+     *
+     * @param args
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String lahto, paatto;
+        System.out.println("Anna aloitus pysäkki:");
+        lahto = lukija.nextLine();
+        System.out.println("Anna päätös pysäkki:");
+        paatto = lukija.nextLine();
+        
+        kulkuneuvo();
+    }
+    
+    static public int matka(LinkedList kulkuneuvo, String lahto, String paatto) {
+        int maara = 0;
+        
+    
+        
+        return maara;
+    }
+    
+    /**
+     *Toistaiseksi asemat lisätään yksitellen, tähän koitan keksiä
+     * paremman ratkaisun
+     */
+    static public void kulkuneuvo() {
+        LinkedList metro = new LinkedList();
+        pysakki(metro, "Ruoholahti");
+        pysakki(metro, "Kamppi");
+        pysakki(metro, "Rautatieasema");   
+        pysakki(metro, "Kaisaniemi");
+        pysakki(metro, "Hakaniemi");
+        pysakki(metro, "Sörnäinen");
+        pysakki(metro, "Kalasatama");
+        pysakki(metro, "Siilitie");
+
+        int index= 0;
+        while(index < metro.size()) {
+            System.out.println(metro.get(index));
+            index++;
+        }
+    }
+    
+    /**
+     *
+     * @param kulkuneuvo
+     * @param asema
+     * 
+     */
+    static public void pysakki(LinkedList kulkuneuvo, String asema) {
+        kulkuneuvo.add(asema);
     }
 }
+
