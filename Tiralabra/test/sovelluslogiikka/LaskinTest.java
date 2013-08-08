bcd75f1c4e482695b446190afa554b7c6bf587bb
Jukka Pitkanen <jukka.pitkanen@helsinki.fi>
2013-08-08 23:47:59 +0300

    paivitysta

---
+++ Tiralabra/test/sovelluslogiikka/LaskinTest.java
@@ -0,0 +1,33 @@
+package sovelluslogiikka;
+
+//import org.junit.After;
+//import org.junit.AfterClass;
+import org.junit.Before;
+//import org.junit.BeforeClass;
+import org.junit.Test;
+//import static org.junit.Assert.*;
+
+public class LaskinTest {
+
+    Laskin laskin;
+
+//    public LaskinTest() {
+//    }
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    @After
+//    public void tearDown() {
+//    }
+    @Before
+    public void setUp() {
+        laskin = new Laskin();
+    }
+
+    @Test
+    public void hello() {
+    }
+}
