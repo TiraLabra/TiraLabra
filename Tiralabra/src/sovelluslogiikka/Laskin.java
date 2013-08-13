838b423b4103ad281c47ed110958da54e9ec2b15
Jukka Pitkanen <jukkapit@dhp-20.cs.helsinki.fi>
2013-08-13 15:58:02 +0300

    paivitysta

--- Tiralabra/src/sovelluslogiikka/Laskin.java
+++ Tiralabra/src/sovelluslogiikka/Laskin.java
@@ -10,12 +10,20 @@ import java.util.Scanner;
  */
 public class Laskin {
 
-    Scanner lukija = new Scanner(System.in);
+    private Scanner lukija;
+    private String kokonaisluvut;
+
+    private enum OPERANDI {
+
+        PLUS, MIINUS, KERTO, JAKO, SULKUAUKI
+    };
 
     /**
      * Konstruktori
      */
     public Laskin() {
+        lukija = new Scanner(System.in);
+        kokonaisluvut = "0123456789";
     }
 
     /**
@@ -48,21 +56,83 @@ public class Laskin {
     }
 
     /**
-     * Ratkaisee laskutoimituksen, ja ilmoittaa ratkaisun
+     * Ratkaisee parametrina annetun laskutoimituksen, ja ilmoittaa ratkaisun
      *
      * @param syote Merkkijono
      */
     public void ratkaiseLaskutoimitus(String syote) {
         Pino operandit = new Pino(syote.length());
         Pino luvut = new Pino(syote.length());
-        
+
         for (int i = 0; i < syote.length(); i++) {
-            switch(syote.charAt(i)){
+            switch (syote.charAt(i)) {
+                case '(':
+                    operandit.push(OPERANDI.SULKUAUKI);
+                    break;
+                case '*':
+                    operandit.push(OPERANDI.KERTO);
+                    break;
+                case '/':
+                    operandit.push(OPERANDI.JAKO);
+                    break;
                 case '+':
+                    while (!operandit.empty() && (operandit.peek() == OPERANDI.KERTO || operandit.peek() == OPERANDI.JAKO)) {
+                        suoritaOperaatiot(luvut, operandit);
+                    }
+                    operandit.push(OPERANDI.PLUS);
+                    break;
+                case '-':
+                    while (!operandit.empty() && (operandit.peek() == OPERANDI.KERTO || operandit.peek() == OPERANDI.JAKO)) {
+                        suoritaOperaatiot(luvut, operandit);
+                    }
+                    operandit.push(OPERANDI.MIINUS);
+                    break;
+                case ')':
+                    while (operandit.peek() != OPERANDI.SULKUAUKI) {
+                        suoritaOperaatiot(luvut, operandit);
+                    }
+                    OPERANDI operandi = (OPERANDI) operandit.pop();
+                    assert (operandi == OPERANDI.SULKUAUKI);
+                    break;
+                default:
+                    int j = i;
+                    while (i < syote.length() && kokonaisluvut.indexOf(syote.charAt(i)) >= 0) {
+                        i++;
+                    }
+                    int numero = Integer.parseInt(syote.substring(j, i));
+                    luvut.push(numero);
+                    i--;
+                    break;
             }
-            
         }
-        
-        System.out.println("Ratkaisu: ");
+        while (!operandit.empty()) {
+            suoritaOperaatiot(luvut, operandit);
+        }
+        int ratkaisu = (Integer) luvut.pop();
+        assert (luvut.empty());
+        System.out.println("Ratkaisu: " + ratkaisu);
+    }
+
+    /**
+     * Suorittaa parametreina annettuihin pinoihin kertyneet laskuoperaatiot
+     *
+     * @param luvut Kokonaislukupino
+     *
+     * @param operandit Luetellun tyypin pino
+     */
+    public void suoritaOperaatiot(Pino luvut, Pino operandit) {
+        switch ((OPERANDI) operandit.pop()) {
+            case PLUS:
+                luvut.push((Integer) luvut.pop() + (Integer) luvut.pop());
+                break;
+            case KERTO:
+                luvut.push((Integer) luvut.pop() * (Integer) luvut.pop());
+                break;
+            case MIINUS:
+                int luku1 = (Integer) luvut.pop();
+                int luku2 = (Integer) luvut.pop();
+                luvut.push(luku2 - luku1);
+                break;
+        }
     }
 }
