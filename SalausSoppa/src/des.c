a5f6437b7ceb86813d7dc7c7f9815a2285b0273a
Tero Keinänen <kenkku@kenkku.net>
2013-08-23 01:54:38 +0300

    decryption

--- SalausSoppa/src/des.c
+++ SalausSoppa/src/des.c
@@ -5,9 +5,11 @@
 #include "util.h"
 #include "key_schedule.h"
 
+extern size_t IP[64];
+extern size_t IP_rev[64];
+
 uint64_t des_encrypt(uint64_t input, uint64_t key)
 {
-    extern size_t IP[64];
     input = permute(input, 64, IP);
 
     uint64_t left = input & 0xffffffff00000000ul;
@@ -25,7 +27,27 @@ uint64_t des_encrypt(uint64_t input, uint64_t key)
 
     // left and right are now reversed
     input = right ^ (left >> 32);
+    return permute(input, 64, IP_rev);
+}
 
-    extern size_t IP_rev[64];
+uint64_t des_decrypt(uint64_t input, uint64_t key)
+{
+    input = permute(input, 64, IP);
+
+    uint64_t left = input & 0xffffffff00000000ul;
+    uint64_t right = (input & 0x00000000fffffffful) << 32;
+
+    uint64_t subkeys[SUBKEY_NUM] = 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    generate_subkeys(key, subkeys);
+
+    for(int i = SUBKEY_NUM - 1; i >= 0; i--) {
+        uint64_t old_right = right;
+        right = left ^ feistel(right, subkeys[i]);
+        left = old_right;
+    }
+
+    // left and right are now reversed
+    input = right ^ (left >> 32);
     return permute(input, 64, IP_rev);
 }
--- SalausSoppa/src/des.h
+++ SalausSoppa/src/des.h
@@ -1,3 +1,4 @@
 #include <stdint.h>
 
 uint64_t des_encrypt(uint64_t input, uint64_t key);
+uint64_t des_decrypt(uint64_t input, uint64_t key);
