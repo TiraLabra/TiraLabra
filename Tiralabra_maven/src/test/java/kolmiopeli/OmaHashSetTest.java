fba4bc81e7c5581cc0bff4360770acc2395351ad
unknown <eemi.haukkala@gmail.com>
2013-09-01 18:34:48 +0300

    OmaHashSet testeja kirjoitettu

---
+++ Tiralabra_maven/src/test/java/kolmiopeli/OmaHashSetTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kolmiopeli;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Eemi
+ */
+public class OmaHashSetTest {
+    
+    public OmaHashSetTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
