ca79b6f37753d6e5772c21a4eda3ff6a55ca0277
nkostiai <nikg@tux.fi>
2014-03-14 10:08:17 -0700

    aloitus

---
+++ Docs/Maaritteludokumentti.md
@@ -0,0 +1,3 @@
+Toteutetaan useamman (3-5) jarjestamisalgoritmin implementaatio javalla ja vertaillaan niita keskenaan
+aika- seka tilavaativuuksien kannalta. Lopullisesta tyosta kay ilmi, minkalaiset aika- ja tilavaatimukset
+eri jarjestamisalgoritmeilla on ja missa tilanteessa kutakin algoritmia on tehokkainta kayttaa.
---
+++ Tiralabra_maven/src/main/java/Sorters/MergeSorter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Sorters;
+
+/**
+ *
+ * @author Admin
+ */
+public class MergeSorter {
+    
+    public int[] mergeSort(int[] arrayToSort){
+        
+        
+        
+        
+        return arrayToSort;
+    }
+    
+}
