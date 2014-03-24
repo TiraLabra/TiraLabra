c472575d713987bff12c5a82566463d6b9a9b761
JuhoR <juho.rimpelainen@helsinki.fi>
2014-03-24 19:07:01 +0200

    hienosäätöä perseptroniin

--- Tiralabra_maven/src/main/java/juhor/tiralabra/ui/App.java
+++ Tiralabra_maven/src/main/java/juhor/tiralabra/ui/App.java
@@ -1,4 +1,4 @@
-package juhor.tiralabra_maven;
+package juhor.tiralabra.ui;
 
 import java.util.Arrays;
 import juhor.tiralabra.data_structures.TrainingData;
@@ -12,18 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        double[][] inputVectors = {{0,0},{1,0},{0,1},{1,1}};
+        //Perseptroni oppii AND-operaation:
+        
+        double[][] inputVectors = {{1,0,0},{1,1,0},{1,0,1},{1,1,1}};
         boolean[] outputs = {false,false,false,true};
         TrainingData data = new TrainingData(inputVectors, outputs);
-        Perceptron p = new Perceptron(data, 0, 1);
+        Perceptron p = new Perceptron(data, 0, 0);
         p.learn();
         
         double[] w = p.getWeights();
         
-        double[] vec  = {1,1};
+        double[] vec  = {1,0,1};
         boolean troo = p.test(vec);
-        System.out.println(Arrays.toString(w));
-        System.out.println(troo);
+        System.out.println(Arrays.toString(w)); //tulostetaan painot
+        System.out.println(troo); //tulostetaan tulos
         
     }
 }
