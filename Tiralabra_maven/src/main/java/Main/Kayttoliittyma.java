dbe1b039073c41ba05ef8f8abaf5a6b8467bdc14
nkostiai <nikg@tux.fi>
2014-04-21 22:11:37 +0300

    viikkopalautus uudestaan

--- Tiralabra_maven/src/main/java/Main/Kayttoliittyma.java
+++ Tiralabra_maven/src/main/java/Main/Kayttoliittyma.java
@@ -17,23 +17,12 @@ import java.util.logging.Logger;
  */
 public class Kayttoliittyma {
 
-    private Random randomizer;
-    int small = 1000;
-    int medium = 100000;
-    int large = 10000000;
-    private MergeSorter mergeSorter;
-    private IntroSorter introSorter;
-    private SmoothSorter smoothSorter;
-
+    
     public Kayttoliittyma() {
-        randomizer = new Random();
-        mergeSorter = new MergeSorter();
-        introSorter = new IntroSorter();
-        smoothSorter = new SmoothSorter();
     }
 
     public void kaynnista() {
-        SorterReporter reportteri = new SorterReporter("raportti.txt");
+        SorterReporter reportteri = new SorterReporter("raportti.html");
         reportteri.testSorters();
         try {
             reportteri.exportReport();
@@ -43,104 +32,4 @@ public class Kayttoliittyma {
         }
     }
     
-    /**
-     * Muodostaa ja järjestää annetun kokoisen satunnaistaulukon kaikilla järjestämisalgoritmeillä.
-     * @param koko Haluttu koko.
-     */
-    public void jarjestaSatunnaisTaulukko(int koko) {
-        System.out.println("Mergesort...");
-        jarjestaMergesortilla(muodostaTaulukko(koko));
-
-        System.out.println("Introsort...");
-        jarjestaIntrosortilla(muodostaTaulukko(koko));
-
-        System.out.println("Smoothsort...");
-        jarjestaSmoothsortilla(muodostaTaulukko(koko));
-    }
-    
-    /**
-     * Muodostaa ja järjestää annetun kokoisen järjestyksessä olevan taulukon kaikilla järjestämisalgoritmeillä.
-     * @param koko 
-     */
-    public void jarjestaJarjestyksessaOlevaTaulukko(int koko){
-        System.out.println("Mergesort...");
-        jarjestaMergesortilla(muodostaJarjTaulukko(koko));
-
-        System.out.println("Introsort...");
-        jarjestaIntrosortilla(muodostaJarjTaulukko(koko));
-
-        System.out.println("Smoothsort...");
-        jarjestaSmoothsortilla(muodostaJarjTaulukko(koko));
-    }
-    
-    public void jarjestaKaikkiSamojaTaulukko(int koko){
-        System.out.println("Mergesort...");
-        jarjestaMergesortilla(muodostaKaikkiSamojaTaulukko(koko));
-
-        System.out.println("Introsort...");
-        jarjestaIntrosortilla(muodostaKaikkiSamojaTaulukko(koko));
-
-        System.out.println("Smoothsort...");
-        jarjestaSmoothsortilla(muodostaKaikkiSamojaTaulukko(koko));
-    }
-
-    private void jarjestaMergesortilla(int[] jarjestettava) {
-        long aloitus = System.nanoTime();
-        mergeSorter.mergeSort(jarjestettava);
-        long lopetus = System.nanoTime();
-        System.out.println("Mergesortilla aikaa meni: " + ((lopetus - aloitus) / 1000000) + "ms");
-
-    }
-
-    private void jarjestaSmoothsortilla(int[] jarjestettava) {
-        long aloitus = System.nanoTime();
-        smoothSorter.smoothSort(jarjestettava);
-        long lopetus = System.nanoTime();
-        System.out.println("Smoothsortilla aikaa meni: " + ((lopetus - aloitus) / 1000000) + "ms");
-
-    }
-    
-    
-    private void jarjestaIntrosortilla(int[] jarjestettava) {
-        long aloitus = System.nanoTime();
-        introSorter.introSort(jarjestettava);
-        long lopetus = System.nanoTime();
-        System.out.println("Introsortilla aikaa meni: " + ((lopetus - aloitus) / 1000000) + "ms");
-
-    }
-    /**
-     * 
-     * Muodostaa annetun kokoisen taulukon satunnaisgeneraattorilla.
-     * @param koko Taulukon koko.
-     * @return Taulukko.
-     */
-    private int[] muodostaTaulukko(int koko) {
-        int [] uusi = new int[koko];
-        for (int i = 0; i < uusi.length; i++) {
-            uusi[i] = randomizer.nextInt(uusi.length);
-        }
-        return uusi;
-    }
-    /**
-     * Muodostaa annetun kokoisen taulukon järjestyksessä.
-     * 
-     * @param koko Taulukon koko.
-     * @return Taulukko.
-     */
-    private int[] muodostaJarjTaulukko(int koko) {
-        int [] uusi = new int[koko];
-        for (int i = 0; i < uusi.length; i++) {
-            uusi[i] = i;
-        }
-        return uusi;
-    }
-    
-    private int[] muodostaKaikkiSamojaTaulukko(int koko){
-        int [] uusi = new int[koko];
-        for (int i = 0; i < uusi.length; i++) {
-            uusi[i] = 3;
-        }
-        return uusi;
-    }
-
 }
--- Tiralabra_maven/src/main/java/Main/SorterReporter.java
+++ Tiralabra_maven/src/main/java/Main/SorterReporter.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Main;
 
 import Sorters.IntroSorter;
@@ -20,15 +19,18 @@ import java.util.Random;
  * @author Admin
  */
 public class SorterReporter {
+
     String filePath;
     String report;
     String currentType;
+    String header;
+    String footer;
     IntroSorter introSort;
     MergeSorter mergeSort;
     SmoothSorter smoothSort;
     Random randomizer;
-    
-    public SorterReporter(String filePath){
+
+    public SorterReporter(String filePath) {
         this.filePath = filePath;
         this.report = "";
         currentType = "random";
@@ -36,104 +38,160 @@ public class SorterReporter {
         introSort = new IntroSorter();
         mergeSort = new MergeSorter();
         smoothSort = new SmoothSorter();
+        initHTML();
     }
-    
-    public void testSorters(){
+
+    private void initHTML() {
+        header = "<html>\n"
+                + "    <head>\n"
+                + "        <title>TODO supply a title</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width\">\n"
+                + "		<style>\n"
+                + "table,th,td\n"
+                + "{\n"
+                + "border:1px solid black;\n"
+                + "}\n"
+                + "</style>\n"
+                + "    </head>\n"
+                + "    <body>";
+        footer = "</body>\n"
+                + "</html>";
+    }
+
+    public void testSorters() {
         initReport();
         doIntroSort();
         doMergeSort();
         doSmoothSort();
+        
+        this.report += "</table>Järjestyksessä olevat taulukot";
+        this.report += "<table><tr><td>Koko</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
+            this.report += "<th>" + i + "</th>";
+        }
+        this.report += "</tr>\n";
+        this.currentType = "sorted;";
+        doIntroSort();
+        doMergeSort();
+        doSmoothSort();
+        
+        this.report += "</table>Käänteisessä järjestyksessä olevat taulukot";
+        this.report += "<table><tr><td>Koko</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
+            this.report += "<th>" + i + "</th>";
+        }
+        this.report += "</tr>\n";
+        this.currentType = "reversed;";
+        doIntroSort();
+        doMergeSort();
+        doSmoothSort();
+        
+        this.report += "</table>Kaikki samannumeroiset";
+        this.report += "<table><tr><td>Koko</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
+            this.report += "<th>" + i + "</th>";
+        }
+        this.report += "</tr>\n";
+        this.currentType = "allsame;";
+        doIntroSort();
+        doMergeSort();
+        doSmoothSort();
+        this.report += footer;
+        
     }
-    
-    private void doIntroSort(){
-        report += ("-------- INTROSORT -------\n");
-        for(int i = 0; i <= 10000000; i = i+ 100000){
+
+    private void doIntroSort() {
+        this.report += "<tr><td>Aika (Introsort/ms)</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
             long startTime = System.nanoTime();
             introSort.introSort(getArray(i));
             long stopTime = System.nanoTime();
-            long finalTime = (stopTime - startTime)/1000000;
-            report += i+","+finalTime+"\n";
+            long finalTime = (stopTime - startTime) / 1000000;
+            report += "<td>"+ finalTime + "</td>";
         }
+        this.report += "</tr>";
     }
-    
-    private void doMergeSort(){
-        report += ("-------- MERGESORT -------\n");
-        for(int i = 0; i <= 10000000; i = i+ 100000){
+
+    private void doMergeSort() {
+        this.report += "<tr><td>Aika (Mergesort/ms)</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
             long startTime = System.nanoTime();
             mergeSort.mergeSort(getArray(i));
             long stopTime = System.nanoTime();
-            long finalTime = (stopTime - startTime)/1000000;
-            report += i+","+finalTime+"\n";
+            long finalTime = (stopTime - startTime) / 1000000;
+            report += "<td>"+ finalTime + "</td>";
         }
+        this.report += "</tr>";
     }
-    
-    private void doSmoothSort(){
-        report += ("-------- SMOOTHSORT -------\n");
-        for(int i = 0; i <= 10000000; i = i+ 100000){
+
+    private void doSmoothSort() {
+        this.report += "<tr>\n   <td>Aika (Smoothsort/ms)</td>\n";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
             long startTime = System.nanoTime();
             smoothSort.smoothSort(getArray(i));
             long stopTime = System.nanoTime();
-            long finalTime = (stopTime - startTime)/1000000;
-            report += i+","+finalTime+"\n";
+            long finalTime = (stopTime - startTime) / 1000000;
+            report += "   <td>"+ finalTime + "</td>\n";
         }
+        this.report += "</tr>";
     }
-    
-    private void initReport(){
-        this.report = "";
-        this.report += "Algoritmien suorituskykytestaus.\n";
-        for(int i = 0; i <= 1000000; i = i+ 1000){
-            this.report += "      |";
+
+    private void initReport() {
+        this.report = header;
+        this.report += "Algoritmien suorituskykytestaus.\n"
+                + "Satunnaistaulukot:";
+        this.report += "<table><tr><td>Koko</td>";
+        for (int i = 1000000; i <= 10000000; i = i + 1000000) {
+            this.report += "<th>" + i + "</th>";
         }
-        this.report += "\n";
+        this.report += "</tr>\n";
     }
-    
-    public void exportReport() throws IOException{
+
+    public void exportReport() throws IOException {
         PrintWriter kirjoittaja = new PrintWriter(filePath, "UTF-8");
         kirjoittaja.write(report);
         kirjoittaja.close();
     }
-    
-    private int[] getArray(int length){
-        if(currentType.equals("random")){
+
+    private int[] getArray(int length) {
+        if (currentType.equals("random")) {
             return getRandomArray(length);
-        }
-        else if(currentType.equals("ordered")){
+        } else if (currentType.equals("ordered")) {
             return getOrderedArray(length);
-        }
-        else if(currentType.equals("reversed")){
+        } else if (currentType.equals("reversed")) {
             return getReversedArray(length);
-        }
-        else{
+        } else {
             return getAllSamesArray(length);
         }
     }
-    
+
     private int[] getOrderedArray(int koko) {
-        int [] uusi = new int[koko];
+        int[] uusi = new int[koko];
         for (int i = 0; i < uusi.length; i++) {
             uusi[i] = i;
         }
         return uusi;
     }
-    
-    private int[] getAllSamesArray(int koko){
-        int [] uusi = new int[koko];
+
+    private int[] getAllSamesArray(int koko) {
+        int[] uusi = new int[koko];
         for (int i = 0; i < uusi.length; i++) {
             uusi[i] = 3;
         }
         return uusi;
     }
-    
+
     private int[] getRandomArray(int koko) {
-        int [] uusi = new int[koko];
+        int[] uusi = new int[koko];
         for (int i = 0; i < uusi.length; i++) {
             uusi[i] = randomizer.nextInt(uusi.length);
         }
         return uusi;
     }
-    
+
     private int[] getReversedArray(int koko) {
-        int [] uusi = new int[koko];
+        int[] uusi = new int[koko];
         for (int i = 0; i < uusi.length; i++) {
             uusi[i] = koko - i;
         }
