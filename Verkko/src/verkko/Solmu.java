97674b45f0c044f47b041290a60458afd6c0342d
Mikko Översti <mikko.oversti@helsinki.fi>
2014-05-07 23:41:49 +0300

    Solmu

---
+++ Verkko/src/verkko/Solmu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verkko;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Arvoitusmies
+ */
+public class Solmu {
+
+    private ArrayList<HashMap<Solmu, Integer>> naapurit;
+
+    public ArrayList<HashMap<Solmu, Integer>> getNaapurit() {
+        return naapurit;
+    }
+
+}
