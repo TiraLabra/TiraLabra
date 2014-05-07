de02c15caf7c765bc76992f182ced9059fc52835
Mikko Översti <mikko.oversti@helsinki.fi>
2014-05-07 23:30:02 +0300

    NB projekti luotu yms..

---
+++ Verkko/src/verkko/Verkko.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package verkko;
+
+/**
+ *
+ * @author Arvoitusmies
+ */
+public class Verkko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
