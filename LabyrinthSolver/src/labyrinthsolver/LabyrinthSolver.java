bb9cc88a66ff53716472bab6f29c88ac542e7b80
Juri Kuronen <juri.kuronen@gmail.com>
2014-05-13 15:03:27 +0300

    Initialize

---
+++ LabyrinthSolver/src/labyrinthsolver/LabyrinthSolver.java
@@ -0,0 +1,13 @@
+package labyrinthsolver;
+
+/**
+ * Tietorakenteet ja algoritmit harjoitustyö. Kesä 2014.
+ *
+ * @author Juri Kuronen
+ */
+public class LabyrinthSolver {
+
+    public static void main(String[] args) {
+    }
+
+}
