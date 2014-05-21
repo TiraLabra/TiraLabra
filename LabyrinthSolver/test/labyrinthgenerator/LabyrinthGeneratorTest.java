af407fe8dd4b9c89dc9dbe1325de4fb0d7389858
Juri Kuronen <juri.kuronen@gmail.com>
2014-05-21 12:19:19 +0300

    Add traverse labyrinth methods, add test for spanningness

--- LabyrinthSolver/test/labyrinthgenerator/LabyrinthGeneratorTest.java
+++ LabyrinthSolver/test/labyrinthgenerator/LabyrinthGeneratorTest.java
@@ -8,37 +8,61 @@ import org.junit.Test;
 
 public class LabyrinthGeneratorTest {
 
-    /**
-     * 1 = NORTH, 2 = EAST, 4 = SOUTH, 8 = WEST.
-     */
-    public final byte[] masks = {1, 2, 4, 8};
-
     LabyrinthGenerator lg;
     public Labyrinth labyrinth;
 
     @Before
     public void setUp() {
-        labyrinth = new Labyrinth(10, 10);
+        labyrinth = new Labyrinth(20, 20);
     }
 
     public void traverseLabyrinth(byte[][] labyrinth, boolean[][] visited, int coordinate, int width) {
         visited[coordinate / width][coordinate % width] = true;
 
-        if ((labyrinth[coordinate / width][coordinate % width] & masks[0]) > 0
-                && !visited[coordinate / width - 1][coordinate % width]) {
-            traverseLabyrinth(labyrinth, visited, coordinate - width, width);
-        }
-        if ((labyrinth[coordinate / width][coordinate % width] & masks[1]) > 0
+        if ((labyrinth[coordinate / width][coordinate % width] & 2) > 0
                 && !visited[coordinate / width][coordinate % width + 1]) {
             traverseLabyrinth(labyrinth, visited, coordinate + 1, width);
         }
-        if ((labyrinth[coordinate / width][coordinate % width] & masks[2]) > 0
+        if ((labyrinth[coordinate / width][coordinate % width] & 4) > 0
                 && !visited[coordinate / width + 1][coordinate % width]) {
             traverseLabyrinth(labyrinth, visited, coordinate + width, width);
         }
-        if ((labyrinth[coordinate / width][coordinate % width] & masks[3]) > 0
+        if ((labyrinth[coordinate / width][coordinate % width] & 1) > 0
+                && !visited[coordinate / width - 1][coordinate % width]) {
+            traverseLabyrinth(labyrinth, visited, coordinate - width, width);
+        }
+        if ((labyrinth[coordinate / width][coordinate % width] & 8) > 0
                 && !visited[coordinate / width][coordinate % width - 1]) {
             traverseLabyrinth(labyrinth, visited, coordinate - 1, width);
+        }
+    }
+
+    public void traverseLabyrinthAndSaveEdges(byte[][] lab, byte[][] testLab, boolean[][] visited, int coordinate) {
+        visited[coordinate / labyrinth.width][coordinate % labyrinth.width] = true;
+
+        if ((lab[coordinate / labyrinth.width][coordinate % labyrinth.width] & 2) > 0
+                && !visited[coordinate / labyrinth.width][coordinate % labyrinth.width + 1]) {
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width] |= 2;
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width + 1] |= 8;
+            traverseLabyrinthAndSaveEdges(lab, testLab, visited, coordinate + 1);
+        }
+        if ((lab[coordinate / labyrinth.width][coordinate % labyrinth.width] & 4) > 0
+                && !visited[coordinate / labyrinth.width + 1][coordinate % labyrinth.width]) {
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width] |= 4;
+            testLab[coordinate / labyrinth.width + 1][coordinate % labyrinth.width] |= 1;
+            traverseLabyrinthAndSaveEdges(lab, testLab, visited, coordinate + labyrinth.width);
+        }
+        if ((lab[coordinate / labyrinth.width][coordinate % labyrinth.width] & 1) > 0
+                && !visited[coordinate / labyrinth.width - 1][coordinate % labyrinth.width]) {
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width] |= 1;
+            testLab[coordinate / labyrinth.width - 1][coordinate % labyrinth.width] |= 4;
+            traverseLabyrinthAndSaveEdges(lab, testLab, visited, coordinate - labyrinth.width);
+        }
+        if ((lab[coordinate / labyrinth.width][coordinate % labyrinth.width] & 8) > 0
+                && !visited[coordinate / labyrinth.width][coordinate % labyrinth.width - 1]) {
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width] |= 8;
+            testLab[coordinate / labyrinth.width][coordinate % labyrinth.width - 1] |= 2;
+            traverseLabyrinthAndSaveEdges(lab, testLab, visited, coordinate - 1);
         }
     }
 
@@ -92,19 +116,38 @@ public class LabyrinthGeneratorTest {
     public void listingUnvisitedNeighbors() {
         Assume.assumeNotNull(lg);
         int[][] visited = new int[labyrinth.height][labyrinth.width];
-        assertEquals(2, lg.getListOfUnvisitedNeighbors(0, visited).size());
-        assertEquals(3, lg.getListOfUnvisitedNeighbors(labyrinth.width / 2, visited).size());
-        assertEquals(4, lg.getListOfUnvisitedNeighbors(labyrinth.width + 5, visited).size());
+        assertEquals(2, lg.labyrinth.getListOfUnvisitedNeighbors(0, visited).size());
+        assertEquals(3, lg.labyrinth.getListOfUnvisitedNeighbors(labyrinth.width / 2, visited).size());
+        assertEquals(4, lg.labyrinth.getListOfUnvisitedNeighbors(labyrinth.width + 5, visited).size());
     }
 
     @Test
     public void creatingBigLabyrinth() {
         Assume.assumeNotNull(lg);
-        labyrinth = new Labyrinth(100, 100);
-        lg.newLabyrinth(labyrinth);
+        labyrinth.updateLabyrinth(100, 100);
         long startTime = System.currentTimeMillis();
         algorithmAddsAllCellsToLabyrinth();
         assertTrue(System.currentTimeMillis() - startTime < 1000);
         allCellsAreVisitableInGeneratedLabyrinth();
+    }
+
+    @Test
+    public void labyrinthIsSpanningTree() {
+        Assume.assumeNotNull(lg);
+        lg.generateLabyrinth();
+        /*
+         Start somewhere in the middle.
+         */
+        int coordinate = labyrinth.width * labyrinth.height / 2 + labyrinth.width / 2;
+        Labyrinth testLabyrinth = new Labyrinth(labyrinth.width, labyrinth.height);
+        traverseLabyrinthAndSaveEdges(labyrinth.labyrinth, testLabyrinth.labyrinth, new boolean[labyrinth.width][labyrinth.height], coordinate);
+        int checksum1 = 0, checksum2 = 0;
+        for (int i = 0; i < labyrinth.height; i++) {
+            for (int j = 0; j < labyrinth.width; j++) {
+                checksum1 += labyrinth.labyrinth[i][j];
+                checksum2 += testLabyrinth.labyrinth[i][j];
+            }
+        }
+        assertTrue(checksum1 == checksum2);
     }
 }
