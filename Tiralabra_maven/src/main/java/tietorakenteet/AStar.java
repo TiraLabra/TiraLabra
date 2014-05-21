ef52a45fbafd9226f84849fc738a50822815edce
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2014-05-21 13:15:57 +0300

    Eteenpäin hieman

--- Tiralabra_maven/src/main/java/tietorakenteet/AStar.java
+++ Tiralabra_maven/src/main/java/tietorakenteet/AStar.java
@@ -1,5 +1,9 @@
 package tietorakenteet;
 
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * A Star -reitinhaun luokka.
  */
@@ -8,8 +12,34 @@ public class AStar {
     
     // A-starin rakenteita, oikeat tyypit inttien tilalle määriteltävä...
     private int[] kaydyt;
-    private int[] kaymatta;
+    
+    private TreeSet kaydyt2;
+    //private int[] kaymatta;
+    
+    private PriorityQueue<Node> kaymatta;
+    private SortedSet<Node> kaymatta2;
+    
     private int[] kuljettuReitti;
+
+    public AStar() {
+        kaydyt2 = new TreeSet();
+    }
+    
+    /**
+     *
+     * @param a
+     * @param alku
+     * @param loppu 
+     */
+    public void AStarHaku(Alue a, Node alku, Node loppu) {
+        
+        // Alustetaan 
+        kaymatta.add(alku);
+        
+        
+        
+    }
+    
     
     
 
--- Tiralabra_maven/src/main/java/tietorakenteet/Node.java
+++ Tiralabra_maven/src/main/java/tietorakenteet/Node.java
@@ -4,7 +4,7 @@ package tietorakenteet;
  * Node-luokka yksittäisiä haettavan alueen nodeja varten.
  * Sisältää paikkatiedot, ja A*-algoritmin tarvitsemat tietokentät.
  */
-public class Node {
+public class Node implements Comparable<Node> {
 
     private int x;
     private int y;
@@ -13,15 +13,21 @@ public class Node {
     private int etaisyysMaaliin;
     
     private Node edellinen;
-
+    
+    private int syvyys;
+    
+    private int kustannus;
+    
+    
     /**
      * Konstruktori, joka ottaa parametrainaan x- ja y-koordinaatit.
      * @param x
      * @param y 
      */
-    public Node(int x, int y) {
+    public Node(int x, int y, int kustannus) {
         this.x = x;
         this.y = y;
+        this.kustannus = kustannus;
         
         
     }
@@ -56,6 +62,16 @@ public class Node {
     
     public void setEdellinen(Node edellinen) {
         this.edellinen = edellinen;
+    }
+    
+    /**
+     * Nodejen välisen suuruusvertailut toteuttava metodi
+     * @param node vertailtava toinen node
+     * @return
+     */    
+    public int compareTo(Node node) {
+        
+        return (this.etaisyysAlusta+this.etaisyysMaaliin) - (n.etaisyysAlusta+n.etaisyysMaaliin);
     }
     
     
