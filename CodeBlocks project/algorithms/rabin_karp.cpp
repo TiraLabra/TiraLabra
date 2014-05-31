2454cdc4c0ee533bdbbfa6c5895466c49b2ccc30
Martin <martin.b.radev@gmail.com>
2014-06-01 00:16:52 +0300

    changed rabin karp algorithm to be used as a class

--- CodeBlocks project/algorithms/rabin_karp.cpp
+++ CodeBlocks project/algorithms/rabin_karp.cpp
@@ -1,10 +1,18 @@
 #include "rabin_karp.hpp"
 
-int rabin_karp_search(const string & haystack, const string & needle, int start) {
+rabin_karp_searcher::rabin_karp_searcher() {
+    this->hasher = new bernstein_hasher();
+}
+
+rabin_karp_searcher::~rabin_karp_searcher() {
+    delete[] hasher;
+}
+
+int rabin_karp_searcher::rabin_karp_search(const string & haystack, const string & needle, int start) {
     return rabin_karp_search(haystack.c_str(), needle.c_str(), start, haystack.length(), needle.length());
 }
 
-int rabin_karp_search(const char * haystack, const char * needle, int start, int haystack_length, int needle_length) {
+int rabin_karp_searcher::rabin_karp_search(const char * haystack, const char * needle, int start, int haystack_length, int needle_length) {
     if (haystack_length == -1) {
         haystack_length = strlen(haystack);
     }
@@ -12,7 +20,6 @@ int rabin_karp_search(const char * haystack, const char * needle, int start, int
         needle_length = strlen(needle);
     }
     if (needle_length == 0) return NOT_FOUND;
-    bernstein_hasher * hasher = new bernstein_hasher(33);
     ULL temp_hash = 0;
     ULL needle_hash = hasher->bernstein_hash(needle, needle_length);
     for (int i = start; i < haystack_length; ++i) {
--- CodeBlocks project/algorithms/rabin_karp.hpp
+++ CodeBlocks project/algorithms/rabin_karp.hpp
@@ -7,7 +7,13 @@
 #include "hashing/hashing.hpp"
 using namespace std;
 
-int rabin_karp_search(const string & haystack, const string & needle, int start = 0);
-
-int rabin_karp_search(const char * haystack, const char * needle,
+class rabin_karp_searcher {
+    private:
+    bernstein_hasher * hasher;
+    public:
+    rabin_karp_searcher();
+    ~rabin_karp_searcher();
+    int rabin_karp_search(const string & haystack, const string & needle, int start = 0);
+    int rabin_karp_search(const char * haystack, const char * needle,
                       int start = 0, int haystack_length = -1, int needle_length = -1);
+};
