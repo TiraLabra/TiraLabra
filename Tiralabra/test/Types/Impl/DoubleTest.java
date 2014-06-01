d5c9585d6446b20a02385586b56e47cf1cda7435
Riku Walve <riku.walve@gmail.com>
2014-06-01 14:02:13 +0300

    wrapperi doublelle

---
+++ Tiralabra/test/Types/Impl/DoubleTest.java
@@ -0,0 +1,10 @@
+package Types.Impl;
+
+public class DoubleTest extends NumberTests {
+    @Override
+    public void setUp() {
+        one = new Double(1.);
+        two = new Double(2.);
+        four = new Double(4.);
+    }
+}
