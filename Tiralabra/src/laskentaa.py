434c030f5253126a1d952ef6f8638413857d020a
Markus Kaukonen <markus.kaukonen@iki.fi>
2014-06-03 23:57:27 +0300

    monteCarlo minimointi, test_mc.py minimoi 2:n ytimen ja kahden elektronin systeemin

--- Tiralabra/src/laskentaa.py
+++ Tiralabra/src/laskentaa.py
@@ -4,7 +4,10 @@
 """ Tässä moduulissa laskentaan liittyviä funktioita. Esim. 
 ratkaistaan hartree potentiaali  """
 
-def tee_gauss_seidel_yksi_askel(gridi, varaus, dx, dy, dz):
+import numpy as np
+import energiat
+
+def tee_gauss_seidel_yksi_askel(V_hartree, varaus):
     """ Iteroidaan Poissonin yhtälön (nabla²=-4*pi*rho)
     ratkaisua yksi askel:
     http://en.wikipedia.org/wiki/Discrete_Poisson_equation
@@ -16,6 +19,8 @@ def tee_gauss_seidel_yksi_askel(gridi, varaus, dx, dy, dz):
     http://wiki.scipy.org/PerformancePython
     """
     from math import pi
+    gridi = V_hartree.gridi
+    h = V_hartree.get_grid_step()
     imax = gridi.shape[0]
     jmax = gridi.shape[1]
     kmax = gridi.shape[2]
@@ -29,7 +34,7 @@ def tee_gauss_seidel_yksi_askel(gridi, varaus, dx, dy, dz):
                     gridi[i+1,j,k]*0.25 + \
                     gridi[i,j-1,k]*0.25 + \
                     gridi[i,j+1,k]*0.25 + \
-                    pi * varaus[i,j,k] * dx * dy
+                    pi * varaus.gridi[i,j,k] * h * h
     #3d
     #3d
     else:
@@ -44,5 +49,68 @@ def tee_gauss_seidel_yksi_askel(gridi, varaus, dx, dy, dz):
                         gridi[i+1,j,k]/6 + \
                         gridi[i,j-1,k]/6 + \
                         gridi[i,j+1,k]/6 + \
-                        2. / 3. * pi * varaus[i,j,k] * dx * dy * dz
+                        2. / 3. * pi * varaus.gridi[i,j,k] * h * h * h
+
+
+
+def monte_carlo_yksi_askel_kaikki(elektroni_tiheys, V_hartree,
+                                  ydin_tiheys,
+                                  tiheydenmuutos=0.1, T=100):
+    """ muutetaan varaustiheyttä ja hyväksytään tai hylätään siirto 
+    hyväksytään siirto== muutetaan uuteen tiheyteen
+    hylätään siirto== ei muuteta tiheyttä.
+    Koko varaustiheys muutetaan kerralla (jotta elektronimäärä säilyy)
+    """
+    import math
+    kb = 3.1668114e-6  #Bolztmannin vakio atomiyksiköissä
+
+    def onko_pariton(num):
+        return bool(num % 2)
+
+    #lasketaan kokonaisenergia vanhalla elektronitiheydellä
+    print "vanha",
+    e_vanha = energiat.E_tot(elektroni_tiheys, V_hartree, ydin_tiheys)
+
+    # alkuvarausmuutos, kerrotaan 1-x ja 1+x jossa x muutos (aluksi 10%)
+    size = elektroni_tiheys.gridi.size
+    positiiviset = np.ones(size/2)*(1.+tiheydenmuutos)
+    negatiiviset = np.ones(size/2)*(1.-tiheydenmuutos)
+    #print size
+    #print "POS",positiiviset
+    #print "NEG",negatiiviset    
+    yrite = np.concatenate([positiiviset, negatiiviset])
+    if onko_pariton(size):
+        yksi = np.ones(1)
+        yrite = np.concatenate([yrite, yksi])
+    np.random.shuffle(yrite)
+    #print "elektronitiheyden muutos", yrite
+    elektroni_tiheys_vanha = elektroni_tiheys.gridi
+    yrite = yrite.reshape(elektroni_tiheys.gridi.shape)
+    #print "elektroni_tiheys_vanha", elektroni_tiheys_vanha
+    #print "elektroni_tiheys muutos", yrite
+    elektroni_tiheys.gridi = elektroni_tiheys.gridi * yrite
+    #skaalataan elektronien lukumäärä takaisin alkuperäiseen
+    elektroni_tiheys.gridi = elektroni_tiheys.gridi * \
+        elektroni_tiheys.get_summa_mennyt() / \
+        elektroni_tiheys.get_summa_nykyinen()
+
+    #print "elektroni_tiheys uusi", elektroni_tiheys.to_1d_list()
+    #print "elektronitiheys uusi", elektroni_tiheys.gridi
+    print "uusi",
+    e_uusi = energiat.E_tot(elektroni_tiheys, V_hartree, ydin_tiheys)
+    e_diff = e_uusi - e_vanha
+    print "ediff", e_diff
+    if e_uusi > e_vanha:
+        print "eksponenetti", math.exp(-(e_diff)/(kb*T)), e_diff
+        #print 
+        if np.random.random_sample() > math.exp(-(e_diff)/(kb*T)):
+            #hylätään uusi 
+            elektroni_tiheys.gridi = elektroni_tiheys_vanha
+            #print "eksponenetti", math.exp(-(e_diff)/(kb*T))
+            print "MC hylätty"
+        else:
+            print "MC hyväksytty YLÄMÄKEEN"
+    else:
+        print "MC hyväksytty"
+    
 
