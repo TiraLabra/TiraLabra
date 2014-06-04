8b1edb48efb60f3a875c0664a9e92db7a0737c22
Huttunen Mika M H <mikhuttu@da1-cs-b221-02.helsinki.fi>
2014-06-04 15:20:17 +0300

    Main.java

--- Tiralabra_maven/src/main/java/Toteutus/Main.java
+++ Tiralabra_maven/src/main/java/Toteutus/Main.java
@@ -14,11 +14,32 @@ import java.io.IOException;
 public class Main {
     
     public static void main(String[] args) throws IOException {
-        PakkaamisenOhjaaja ohjaaja = new PakkaamisenOhjaaja();
-        ohjaaja.suoritaPakkaaminen("abcdef.txt");
+//        PakkaamisenOhjaaja ohjaaja = new PakkaamisenOhjaaja();
+//        ohjaaja.suoritaPakkaaminen("abcdaa.txt");
         
         
-//        TiedostonPurkaja purkaja = new TiedostonPurkaja();
-//        purkaja.pura("abcd.txt.hemi");
+        TiedostonPurkaja purkaja = new TiedostonPurkaja();
+        purkaja.pura("abcdaa.txt");
     }
 }
+
+
+
+// 00010111001100
+// b c  g f a  b
+
+// Huffman puun tekstiesitys suoraan "BittiEsityksistä".
+
+//     a = 011
+//     b = 00
+//     c = 010
+//     f = 10
+//     g = 11
+
+// Jonoa ei tarvitse toteuttaa laisinkaan. StringBuilderia saa käyttää.
+// Purkaminen tapahtuu siten että luetaan Huffman puu (= Bittiesitykset) läpi, viedään ne hajautustauluun (bittijono --> char) ja
+// tämän jälkeen puretaan tiedosto lukemalla bitti bitiltä. Jos ei löydy vastinetta haj. taulusta, luetaan lisää.
+
+// Jos Huffman puun tallettaa tiedoston alkuun loppuun, ei tarvitse muodostaa pointteria laisinkaan.
+// Puu kannattaa tallettaa siten että jokaisen merkin perään lisätään tavuja 00 ja 01 (vastaamaan 0 ja 1 bittejä siitä, millainen esitys merkillä on). Ei vie liikaa tilaa :)
+// Puu päättyy siten että sen lopussa on esim. FF FF koska jokaista merkkiä vastaava seur. merkki pitäisi olla 00 tai 01.
