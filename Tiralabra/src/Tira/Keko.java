51ee212377d0548b2a96bb5a2104c1873e5aab9d
Esko Niiranen <eniirane@kaihoapina.lan>
2014-06-09 12:42:02 +0300

    lisää sisältöä kekoon

--- Tiralabra/src/Tira/Keko.java
+++ Tiralabra/src/Tira/Keko.java
@@ -9,8 +9,59 @@ package Tira;
  * @author eniirane
  */
 public class Keko<V> {
-    public Keko() {
+    
+    int pituus = 0;
+    int koko = 0;
+    
+    Lista lista;
+    
+    public Keko(int pituus) {
+        this.pituus = pituus;
+        Lista lista = new Lista();
     }
     
+    public void insert(V value) {
+    }
     
+    public void min() {
+        //todo public V 
+    }
+    
+    public void del_min() {
+    
+    }
+
+    public void heapify(int i) {
+        int l = getLeft(i);
+        int r = getRight(i);
+        int pienempi = Math.min( getIndex(l), getIndex(r) );
+        if (r <= this.koko) {
+            if (getIndex(i) > pienempi) {
+                vaihda(getIndex(i), getIndex(pienempi));
+                heapify(pienempi);
+            }
+        } else if (l == this.koko && getIndex(i) > getIndex(l)) {
+                vaihda(getIndex(i), getIndex(l));
+        }
+    }
+
+    public void vaihda(int i, int j) {
+    
+    }
+    
+    public int getIndex(int i) {
+        return -1;
+    }
+    
+    public int getParent(int i) {
+        return (int)Math.floor(i/2);
+    }
+    
+    public int getLeft(int i) {
+        return i*2;
+    }
+    
+    public int getRight(int i) {
+        return (i*2) + 1;
+    }
 }
--- Tiralabra/src/Tira/Lista.java
+++ Tiralabra/src/Tira/Lista.java
@@ -9,8 +9,19 @@ package Tira;
  * @author eniirane
  */
 public class Lista<V> {
+    public V first;
     
     public Lista() {
+        this.first = null;
+    }
+    
+    public void add(V value) {
+        
+    }
+    
+    public int get(int i) {
+        return -1;
     }
     
 }
+
