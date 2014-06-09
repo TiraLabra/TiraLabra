3510d32891d3730ab2b770ceec49e9f76b016b83
Nicolas Arkkila <nicolas@arkkila.fi>
2014-06-09 18:32:56 +0300

    Word array implemented

--- nmd5.py
+++ nmd5.py
@@ -20,7 +20,7 @@ class NMD5:
 
 	def H(x, y, z):
 		"""X xor Y xor Z"""
-		return x ^ y ^ z
+		return x ^ y ^ zeroes
 
 	def I(x, y, z):
 		"""Y xor (X v not(Z))"""
@@ -32,7 +32,6 @@ class NMD5:
 	def update(arg):
 		list.add(Node(arg, None))
 
-
     # Main hashing function
 	def hash(self, message):
 		a = self.A
@@ -40,18 +39,20 @@ class NMD5:
 		c = self.C
 		d = self.D
 
-		words = self.splitToBlocks(self.pad(self.toBinaryString(message)), 16)
 
-		for word in words:
+		chunks = self.splitToBlocks(self.pad(self.toBinaryString(message)), 512)
 
-			self.R1(a,b,c,d,int(words[0], 2),7,0xD76AA478);
-			#print(word)
-			# Round 1
-			# Round 2
-			# Round 3
-			# Round 4
-		#return AA + BB + CC + DD
-		return a
+		for chunk in chunks:
+			AA = self.A
+			BB = self.B
+			CC = self.C
+			DD = self.D
+
+			words = self.createWordArray(message)
+
+			for word in words:
+
+		return "wew"
 
 	def toBinaryString(self, string):
 		"""Converts a given string into a binary representation of itself"""
@@ -94,8 +95,8 @@ class NMD5:
 		padded = ''
 
 		padded = "0" * (64 - len(s))
-		padded += s
-		return padded
+		padded += s[::-1] # reverse length byte first to preserve correct order
+		return padded[::-1]
 
 
 	def splitToBlocks(self, message, n):
@@ -104,6 +105,34 @@ class NMD5:
 		if n<1:
 			n=1
 		return [message[i:i+n] for i in range(0, len(message), n)]
+
+	def createWordArray(self, message):
+		"""Primes the 16-bit words for the main function"""
+		
+		message = self.splitToBlocks(self.pad(self.toBinaryString(message)), 32)
+		wordArray = [0] * 16 # Our 16-word array
+
+		wordIndex = 0
+
+		for word in message:
+			bytes = self.splitToBlocks(word, 8)
+			tempByte = 0
+			powers = 0
+			byteI = 0
+
+			for byte in bytes:
+				#print("Wordindex:", wordIndex)
+				tempByte = wordArray[wordIndex]
+				#print(tempByte, " | ", int(byte, 2), " << ", powers)
+				tempByte = tempByte | int(byte, 2) << powers
+				powers += 8
+				wordArray[wordIndex] = tempByte
+				#print(wordArray[wordIndex])
+			
+			wordIndex += 1
+			powers = 0
+
+		return wordArray
 
 ## Public methods TBI ##
 
