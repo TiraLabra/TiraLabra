7d0d7f625bd879fa1e81118a9a739ce2e505a437
Riku Walve <riku.walve@gmail.com>
2014-06-11 00:23:35 +0300

    alotettu reaalilukujen toteutus

--- Tiralabra/src/TiraLabra/Number/Real.java
+++ Tiralabra/src/TiraLabra/Number/Real.java
@@ -1,113 +1,159 @@
 package TiraLabra.Number;
 
-import java.math.BigDecimal;
-
 /**
  * Mielivaltaisen tarkka reaaliluku
  * @author riku
  */
 public class Real extends Number<Real> {
-    public static final Real ONE = new Real(BigDecimal.ONE);
-    public static final Real ZERO = new Real(BigDecimal.ZERO);
-    public static final Real TEN = new Real(BigDecimal.TEN);
+    public static final Real ONE = new Real(Integer.ONE, 0);
+    public static final Real ZERO = new Real(Integer.ZERO, 0);
+    public static final Real TEN = new Real(Integer.TEN, 0);
     
-    // Tarpeettoman isot epsilonit sulla
-    public final static Real EPSILON = new Real(1e-2);
+    public static final Real EPSILON = new Real(Integer.ONE, 10);
     
-    public final BigDecimal decimal;
-    
-    /**
-     * Luo luvun doublesta
-     * @param decimal
-     */
-    public Real(double decimal) {
-        this.decimal = BigDecimal.valueOf(decimal);
-    }
+    protected final Integer mantissa;
+    protected final int exponent;
     
     /**
      * Luo luvun kokonaisluvusta
      * @param integer
      */
     public Real(int integer) {
-        this.decimal = BigDecimal.valueOf(integer);
+        mantissa = new Integer(integer);
+        exponent = 0;
     }
-
-    /**
-     * Luo luvun BigDecimalista
-     * @param decimal BigDecimal-desimaaliluku
-     */
-    public Real(BigDecimal decimal) {
-        this.decimal = decimal;
-    }
-
     
+    protected Real(Integer mantissa, int exponent) {
+        this.mantissa = mantissa;
+        this.exponent = exponent;
+    }
+   
+    @Override
+    public String toString() {
+        boolean neg = mantissa.isNegative();
+        
+        String m = mantissa.abs().toString();
+        if (exponent == 0) {
+            return (neg ? "-" : "") + m;
+        } else {
+            int diff = exponent - m.length() + 1;
+            for (int i = 0; i < diff; i++) {
+                m = "0" + m;
+            }
+            
+            return (neg ? "-" : "")
+                    + m.substring(0, m.length() - exponent)
+                    + "." + m.substring(m.length() - exponent);
+        }
+    }
+
     @Override
     public Real add(Real other) {
-        return new Real(decimal.add(other.decimal));
+        if (this.exponent < other.exponent) {
+            return other.add(this);
+        }
+        
+        int s = this.exponent - other.exponent;
+        Integer shifted = other.mantissa.multiply(Integer.TEN.pow(s));
+        return new Real(mantissa.add(shifted), exponent);
     }
 
     @Override
     public Real subtract(Real other) {
-        return new Real(decimal.subtract(other.decimal));
+        if (this.exponent < other.exponent) {
+            return other.subtract(this).negate();
+        }
+        
+        int s = this.exponent - other.exponent;
+        Integer shifted = other.mantissa.multiply(Integer.TEN.pow(s));
+        return new Real(mantissa.subtract(shifted), exponent);
     }
 
     @Override
     public Real multiply(Real other) {
-        return new Real(decimal.multiply(other.decimal));
+        return new Real(mantissa.multiply(other.mantissa),
+                exponent + other.exponent);
     }
 
     @Override
     public Real divide(Real other) {
-        return new Real(decimal.divide(other.decimal));
+        int exp = exponent + other.exponent + 2;
+        Integer m = mantissa.multiply(Integer.TEN.pow(exp));
+        return new Real(m.divide(other.mantissa), exp);
     }
 
     @Override
     public Real pow(int n) {
-        return new Real(decimal.pow(n));
+        return new Real(mantissa.pow(n), exponent);
+    }
+
+    private boolean isOne() {
+        String s = mantissa.toString();
+        
+        if (s.charAt(0) != '1') {
+            return false;
+        }
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(1) != '0') {
+                return false;
+            }
+        }
+        
+        return true;
     }
     
     @Override
     public Real sqrt() {
-        double root = Math.sqrt(decimal.doubleValue()); // !!
-        return new Real(root);
+        if (this.isOne()) {
+            return this;
+        }
+        
+        int exp = exponent * exponent;
+        Integer m = mantissa.multiply(Integer.TEN.pow(exp));
+        return new Real(m.sqrt(), exp);
+    }
+
+    @Override
+    public boolean isNegative() {
+        return mantissa.isNegative();
+    }
+
+    @Override
+    public boolean isZero() {
+        return mantissa.isZero();
     }
 
     @Override
     public Real abs() {
-        return new Real(decimal.abs());
-    }
-    
-    @Override
-    public Real negate() {
-        return new Real(decimal.negate());
-    }
-    
-    @Override
-    public boolean isNegative() {
-        return (decimal.doubleValue() < 0.);
-    }
-    
-    @Override
-    public boolean isZero() {
-        return this.equals(ZERO);
-    }
-    
-    @Override
-    public String toString() {
-        return decimal.toString();
+        return new Real(mantissa.abs(), exponent);
     }
 
+    @Override
+    public Real negate() {
+        return new Real(mantissa.negate(), exponent);
+    }
+
+    @Override
+    public int compareTo(Real other) {
+        if (this.exponent < other.exponent) {
+            return -other.compareTo(this);
+        }
+        
+        int s = this.exponent - other.exponent;
+        Integer shifted = other.mantissa.multiply(Integer.TEN.pow(s));
+        return mantissa.compareTo(shifted);
+    }
+    
     @Override
     public boolean equals(Object obj) {
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
+        
         final Real other = (Real) obj;
-        return (this.subtract(other).abs().compareTo(EPSILON) < 0);
-    }
-    
-    @Override
-    public int compareTo(Real other) {
-        return decimal.compareTo(other.decimal);
+        
+        Real diff = this.subtract(other).abs();
+        return (diff.compareTo(EPSILON) < 0);
     }
 }
