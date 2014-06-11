d76e96b1972c72c08e8abc91537d22e3cdba4a84
Nicolas Arkkila <nicolas@arkkila.fi>
2014-06-11 21:54:18 +0300

    Hash function now working with multiple blocks

--- nmd5.py
+++ nmd5.py
@@ -81,15 +81,19 @@ class NMD5:
 	D = 0x10325476
 	buffers = [0, 0, 0, 0]
 
-	def update(arg):
-		list.add(Node(arg, None))
+	def update(self, arg):
+		self.list.add(Node(arg, None))
 
 	# Main hashing function
-	def hash(self, message):
+	def __hash(self, message):
 		messageLength = len(message)
 		chunks = self.splitToBlocks(self.pad(self.toBinaryString(message)), 512)
+		#words = self.createWordArray(self.pad(self.toBinaryString(message)), messageLength)
+		#print(words)
 
 		for chunk in chunks:
+			words = self.createWordArray(chunk, messageLength, chunks.index(chunk)==len(chunks)-1)
+			print(words)
 			# Rotation constants
 			R11, R12, R13, R14 = 7, 12, 17, 22
 			R21, R22, R23, R24 = 5, 9, 14, 20
@@ -97,8 +101,6 @@ class NMD5:
 			R41, R42, R43, R44 = 6, 10, 15, 21
 
 			a, b, c, d = A, B, C, D = self.A, self.B, self.C, self.D
-
-			words = self.createWordArray(chunk, messageLength)
 
 			# Round 1
 			a = FF(a, b, c, d, words[ 0], R11, 0xD76AA478)
@@ -171,19 +173,35 @@ class NMD5:
 			d = II(d, a, b, c, words[11], R42, 0xBD3AF235) 
 			c = II(c, d, a, b, words[ 2], R43, 0x2AD7D2BB) 
 			b = II(b, c, d, a, words[ 9], R44, 0xEB86D391)
+			
+			A = (a + A) % pow(2,32)
+			B = (b + B) % pow(2,32)
+			C = (c + C) % pow(2,32)
+			D = (d + D) % pow(2,32)
+			print((a + A) % pow(2,32))
+			print((b + B) % pow(2,32))
+			print((c + C) % pow(2,32))
+			print((d + D) % pow(2,32))
 
-			self.buffers[0] = (a + A) % pow(2,32)
-			self.buffers[1] = (b + B) % pow(2,32)
-			self.buffers[2] = (c + C) % pow(2,32)
-			self.buffers[3] = (d + D) % pow(2,32)
+			self.A = A
+			self.B = B
+			self.C = C
+			self.D = D
+			print("Constants are:")
+			print(self.A)
+			print(self.B)
+			print(self.C)
+			print(self.D)
+
 
 
 	def hexdigest(self):
 		"""Returns hex string of result"""
 		res = ""
+		buffers = [self.A, self.B, self.C, self.D]
 
 
-		for buffer in self.buffers:
+		for buffer in buffers:
 			for i in range(0,4):
 				res = res + "{:02x}".format((buffer >> i*8) & 255)
 
@@ -244,12 +262,15 @@ class NMD5:
 			n=1
 		return [message[i:i+n] for i in range(0, len(message), n)]
 
-	def createWordArray(self, message, messageLength):
+	def createWordArray(self, message, messageLength, finalBlock):
+		print(finalBlock)
 		"""Primes the 16-bit words for the main function"""
+
 		message = self.splitToBlocks(message, 32)
+		print(messageLength)
 		totalBytes = 0
 		totalWords = 0
-		wordArray = [0] * 16 # Our 16-word array
+		wordArray = [0] * 16
 
 		wordIndex = 0
 		for word in message:
@@ -268,8 +289,11 @@ class NMD5:
 			powers = 0
 
 		## correct last two bytes
-		wordArray[-2] = messageLength << 3
-		wordArray[-1] = messageLength >> 29
+		if finalBlock:
+			print("Final block!")
+			wordArray[-2] = messageLength << 3
+			print(messageLength << 3)
+			wordArray[-1] = messageLength >> 29
 
 		return wordArray
 
