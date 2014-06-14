e970ca94c9a1d1680d2bfccde3105723ecb1aa24
Petri <Petri.pihlajaniemi@gmail.com>
2014-06-14 15:07:53 +0300

    Kokeillaan edgeluokkaa

---
+++ Tiralabra/src/Hike/Graph/Edge.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hike.Graph;
+
+/**
+ *
+ * @author petri
+ */
+public class Edge {
+    
+}
