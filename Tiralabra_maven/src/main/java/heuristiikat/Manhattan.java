e463d2d818eeb47200596410fd92c55d9065d0b6
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2014-06-15 20:09:53 +0300

    Manhattan huomioi, että yhden askeleen kustannus on 10

--- Tiralabra_maven/src/main/java/heuristiikat/Manhattan.java
+++ Tiralabra_maven/src/main/java/heuristiikat/Manhattan.java
@@ -10,9 +10,11 @@ import tietorakenteet.Node;
  */
 public class Manhattan implements Heuristiikka {
 
+    private int askel = 10;
+    
     public int laskeArvio(Node alku, Node loppu) {
         int tulos = 0;
-        tulos = Math.abs(alku.getRivi()-loppu.getRivi()) + Math.abs(alku.getSarake()-loppu.getSarake());
+        tulos = Math.abs(alku.getRivi()-loppu.getRivi())*askel + Math.abs(alku.getSarake()-loppu.getSarake())*askel;
         
         return tulos;
     }
