96933e123443e55285626c747327466b362a2de9
Mika M H Huttunen <mikhuttu@z1-3.cs.helsinki.fi>
2014-06-16 12:01:12 +0300

    17.6

--- Tiralabra_maven/src/main/java/Main.java
+++ Tiralabra_maven/src/main/java/Main.java
@@ -1,21 +1,24 @@
 
-//import Tietorakenteet.HajTaulu;
+import Tietorakenteet.HajTaulu;
 import Toteutus.Huffman.Pakkaaminen.PakkaamisenOhjaaja;
 import Toteutus.Huffman.Purkaminen.TiedostonPurkaja;
 import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Random;
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
+
+// Testaa että tiedostoon kirjoittamisessa bittijonot kirjoitetaan optimaalisessa muodossa. Bittiesitykset merkeille ovat kuitenkin täysin ok ja Huffman puu toimii niin kuin pitääkin.
+// Taulukoista, jotka sisältävät suorituskykytestejä, saa kivan näköiset graafiset versiot Google Docsin "Spreadsheetiä" käyttäen.
 
 public class Main {
     
     public static void main(String[] args) throws IOException {
-//        if (true) {
-//            hajautusAjat();
-//            return;
-//        }
+        if (true) {
+            hajautusAjat();
+            return;
+        }
 
-        System.out.println("Anna k�sitelt�v�n tiedoston nimi joka sij. kansiossa /Tiralabra_maven");
+        System.out.println("Anna k�sitelt�v�n tiedoston nimi joka sij. kansiossa /Tiralabra_maven");
         System.out.print("Nimi: ");
         
         Scanner lukija = new Scanner(System.in);
@@ -56,94 +59,96 @@ public class Main {
     private static void pura(String polku) throws IOException {
         new TiedostonPurkaja().pura(polku);
     }   
-}
+
 
     
-//    public static void hajautusAjat() {
-//        HashMap<String, String> hashmap = new HashMap<>();
-//        HajTaulu hajautus = new HajTaulu();
-//        
-//        int maara = 100000;
-//        
-//        String[] map = new String[maara];
-//        String[] taulu = new String[maara];
-//        
-//        long ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            String bittijono = arvoMerkkiJono();
-//            map[i] = bittijono;
-//            hashmap.put(bittijono, "");
-//        }
-//        
-//        long jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//        
-//        
-//        ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            String bittijono = arvoMerkkiJono();
-//            taulu[i] = bittijono;
-//            hajautus.lisaa(bittijono, "");
-//        }
-//        
-//        jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//        
-//        
-//        
-//        ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            hashmap.containsKey(map[i]);
-//        }
-//        
-//        jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//        
-//        
-//        ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            hajautus.sisaltaaAvaimen(taulu[i]);
-//        }
-//        
-//        jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//        
-//        
-//        
-//        
-//        ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            hashmap.remove(map[i]);
-//        }
-//        
-//        jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//        
-//        
-//        ennen = System.currentTimeMillis();
-//        
-//        for (int i = 0; i < maara; i++) {
-//            hajautus.poista(taulu[i]);
-//        }
-//        
-//        jalkeen = System.currentTimeMillis();
-//        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
-//    }
-//    
-//    
-//    private static String arvoMerkkiJono() {
-//        String sana = "";
-//        Random random = new Random();
-//        
-//        int pituus = random.nextInt(20);
-//        
-//        for (int i = 0; i < pituus; i++) {
-//            sana += (char) random.nextInt(256);
-//        }
-//        return sana;
-//    }
+    public static void hajautusAjat() {
+        HashMap<String, String> hashmap = new HashMap<>();
+        HajTaulu hajautus = new HajTaulu();
+        
+        int maara = 100000;
+        
+        String[] map = new String[maara];
+        String[] taulu = new String[maara];
+        
+        long ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            String bittijono = arvoMerkkiJono();
+            map[i] = bittijono;
+            hashmap.put(bittijono, "");
+        }
+        
+        long jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+        
+        
+        ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            String bittijono = arvoMerkkiJono();
+            taulu[i] = bittijono;
+            hajautus.lisaa(bittijono, "");
+        }
+        
+        jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+        
+        
+        
+        ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            hashmap.containsKey(map[i]);
+        }
+        
+        jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+        
+        
+        ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            hajautus.sisaltaaAvaimen(taulu[i]);
+        }
+        
+        jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+        
+        
+        
+        
+        ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            hashmap.remove(map[i]);
+        }
+        
+        jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+        
+        
+        ennen = System.currentTimeMillis();
+        
+        for (int i = 0; i < maara; i++) {
+            hajautus.poista(taulu[i]);
+        }
+        
+        jalkeen = System.currentTimeMillis();
+        System.out.println("Operaatioon kului aikaa " + (jalkeen - ennen) + "ms");
+    }
+    
+    
+    private static String arvoMerkkiJono() {
+        String sana = "";
+        Random random = new Random();
+        
+        int pituus = random.nextInt(20);
+        
+        for (int i = 0; i < pituus; i++) {
+            sana += (char) random.nextInt(256);
+        }
+        return sana;
+    }
+    
+}
--- Tiralabra_maven/src/test/java/Toteutus/Huffman/BittiEsityksetTest.java
+++ Tiralabra_maven/src/test/java/Toteutus/Huffman/BittiEsityksetTest.java
@@ -1,21 +1,25 @@
 package Toteutus.Huffman;
 
+import Apuvalineet.TekstinLukija;
 import Tietorakenteet.Solmu;
+import java.io.FileNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BittiEsityksetTest {
     private BittiEsitykset esitykset;
+    private BittiEsitykset esitykset2;
     private Solmu solmu;
     
     @Before
-    public void setUp() throws Exception {
-        this.esitykset = new BittiEsitykset();
+    public void setUp() {
         alustaBittiEsitykset();
     }
     
-    private void alustaBittiEsitykset() throws Exception {
+    private void alustaBittiEsitykset() {
+        esitykset = new BittiEsitykset();
+        
         this.solmu = new Solmu(6);
         solmu.setVasen(new Solmu("a", 3));
         solmu.setOikea(new Solmu(3));
@@ -27,15 +31,17 @@ public class BittiEsityksetTest {
         esitykset.muodostaMerkeilleBittiEsitykset(solmu, "");
     }
     
+
     @Test
-    public void esityksetTasmaavatPuussa() throws Exception {
+    public void esityksetTasmaavatPuussa1() {
         assertEquals(esitykset.getEsitykset().getArvo("a"), (char) 0 + "");
-        assertEquals(esitykset.getEsitykset().getArvo("b"), (char) 1 + "" + (char) 0);
-        assertEquals(esitykset.getEsitykset().getArvo("c"), (char) 1 + "" + (char) 1);
+        assertEquals(esitykset.getEsitykset().getArvo("b"), (char) 1 + "" + (char) 0 + "");
+        assertEquals(esitykset.getEsitykset().getArvo("c"), (char) 1 + "" + (char) 1 + "");
     }
     
+
     @Test
-    public void huffmanPuunTekstiEsitys() throws Exception {
+    public void huffmanPuunTekstiEsitys() {
         StringBuilder teksti = new StringBuilder();
         for (String avain : esitykset.getEsitykset().getAvaimet()) {
             teksti.append(avain);
@@ -46,5 +52,30 @@ public class BittiEsityksetTest {
         teksti.append((char) 127);
         
         assertEquals(teksti.toString(), esitykset.huffmanPuunTekstiEsitys());
+        assertEquals("a" + (char) 0 + "" + "b" + (char) 1 + "" + (char) 0 + "" + "c" + (char) 1 + "" + (char) 1 + "" + (char) 127 + "" + (char) 127 + "",
+                    esitykset.huffmanPuunTekstiEsitys());
+    }
+    
+    @Test
+    public void bittijonoSamanPituinen() throws FileNotFoundException {
+        TekstinLukija lukija = new TekstinLukija();
+        lukija.lueTiedosto("HuffmanCodingtest.txt");
+        
+        HuffmanPuu puu = new HuffmanPuu();
+        puu.muodostaHuffmanPuu(lukija.getEsiintymat());
+        
+        
+        esitykset2 = new BittiEsitykset();
+        esitykset2.muodostaMerkeilleBittiEsitykset(puu.getKeko().getSolmut()[0], "");
+        
+        String[] avaimet = esitykset2.getEsitykset().getAvaimet();
+        int maara = 0;
+        
+        for (String avain : avaimet) {
+            maara += Integer.parseInt(lukija.getEsiintymat().getArvo(avain)) * esitykset2.getEsitykset().getArvo(avain).length();
+            
+        }
+        
+        assertEquals(135, maara);
     }
 }
