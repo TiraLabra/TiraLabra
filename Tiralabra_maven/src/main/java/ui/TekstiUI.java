b67441dc251a1dd9c8b1c38cf5a51b6976f753b1
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2014-06-18 13:44:39 +0300

    TekstiUI:ta eteenpäin

--- Tiralabra_maven/src/main/java/ui/TekstiUI.java
+++ Tiralabra_maven/src/main/java/ui/TekstiUI.java
@@ -1,5 +1,7 @@
 package ui;
 
+import apurakenteet.Kuvalukija;
+import apurakenteet.Kuvanayttaja;
 import hakualgoritmit.AStar;
 import heuristiikat.Dijkstra;
 import heuristiikat.Euklidinen;
@@ -21,6 +23,14 @@ public class TekstiUI {
     
     private Alue alue;
     
+    private String tiedostonimi;
+    private int alkurivi;
+    private int alkusarake;
+    private int loppurivi;
+    private int loppusarake;
+    
+    private Kuvalukija kl;
+    
     public TekstiUI() {
     }
     
@@ -30,12 +40,13 @@ public class TekstiUI {
 //        String syote = scanner.nextLine();
         String syote;
         
-        while (true) {
+        boolean jatketaan = true;
+        while (jatketaan) {
             alkuvalikko();
             syote = scanner.nextLine();
             if (syote.equalsIgnoreCase("Q")) {
                 lopetus();
-                break;
+                jatketaan = false;
             } else if (syote.equalsIgnoreCase("1")) {
                 kartanValinta();
             } else if (syote.equalsIgnoreCase("2")) {
@@ -52,15 +63,6 @@ public class TekstiUI {
         }
     }
     
-    
-    
-    
- 
-    
-
-    
-    
-    
     private void alkuvalikko() {
         tulostaValitut();
         System.out.println("1 = Valitse kartta-alue");
@@ -75,11 +77,37 @@ public class TekstiUI {
     }
 
     private void suoritaHaku() {
-        System.out.println("Tähän tulee varsinaisen haun suoritus...");
+        astar = new AStar(heuristiikka);
+        astar.AStarHaku(alue, alue.getnode(alkurivi, alkusarake), alue.getnode(loppurivi,loppusarake));
     }
 
     private void kartanValinta() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Anna halutun tiedoston nimi (oletuksena 100x100.bmp):");
+        String syote = scanner.nextLine();
+        if (syote.equals("")) {
+            tiedostonimi = "100x100.bmp";
+            System.out.println("tänne");
+        } else {
+            tiedostonimi = syote;       // Ei mitään tarkastuksia vielä...
+            System.out.println(tiedostonimi);
+        }
+        kl = new Kuvalukija(tiedostonimi);
+        this.alue = new Alue(kl.muodostaAlue(), kl.getKorkeus(), kl.getLeveys());
+        
+        while (true) {
+            System.out.println("Anna alkusolmun rivi:");
+            alkurivi = scanner.nextInt();
+            System.out.println("Anna alkusolmun sarake:");
+            alkusarake = scanner.nextInt();
+            System.out.println("Anna loppusolmun rivi:");
+            loppurivi = scanner.nextInt();
+            System.out.println("Anna loppusolmun sarake:");
+            loppusarake = scanner.nextInt();
+            if (tarkastaKoordinaatit() ) {
+                break;
+            }
+        }
+            
     }
 
     private void heuristiikanValinta() {
@@ -88,22 +116,19 @@ public class TekstiUI {
         System.out.println("2. Manhattan");
         System.out.println("3. Euklidinen");
         String syote = scanner.nextLine();
-        System.out.println("mennään looppiin...");
-        boolean loop = true;
-        while (loop) {
+        while (true) {
             if (syote.equalsIgnoreCase("1")) {
                 heuristiikka = new Dijkstra();
                 System.out.println("Asetettu heuristiikaksi Dijkstra");
-                loop = false;
+                break;
             } else if (syote.equalsIgnoreCase("2")) {
                 heuristiikka = new Manhattan();
                 System.out.println("Asetettu heuristiikaksi Manhattan.");
-                loop = false;
-                return;
+                break;
             } else if (syote.equalsIgnoreCase("3")) {
                 heuristiikka = new Euklidinen();
                 System.out.println("Asetettu heuristiikaksi Euklidinen.");
-                loop = false;
+                break;
             } else
                 System.out.println("Virheellinen valinta");
         }
@@ -112,7 +137,25 @@ public class TekstiUI {
     }
 
     private void tulostaHakutulos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Yhteensä " + astar.getAskelia() + " laskenta-askelta.");
+        
+        ArrayListOma reitti = astar.kerroKuljettuReitti();
+        System.out.println("\nKuljettu reitti: ("+ reitti.koko()+ " kpl)");
+        //for (Node n : reitti) {
+        for (int i = 0; i < reitti.koko(); i++) {
+            Node n = (Node)reitti.palautaKohdasta(i);
+            System.out.println(n.toString());
+            n.toString();
+        }
+        System.out.println(alue.toString());
+        System.out.println(astar.yhteenveto());
+        
+        System.out.println("-------");
+        
+        Kuvanayttaja kn = new Kuvanayttaja(kl.getKuva());
+        kn.muodostaKuvaanPolku(reitti);
+        kn.naytaKuva();
+        
     }
 
     private void tulostaValitut() {
@@ -132,6 +175,11 @@ public class TekstiUI {
 
     private void tulostaAlueentiedot() {
         System.out.println("  Alue: Alue on valittu, TODO: parempi muotoilu");
+    }
+
+    //TODO:
+    private boolean tarkastaKoordinaatit() {
+        return true;
     }
     
 }
