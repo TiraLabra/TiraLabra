f886828f8fd71e3bfffa26c2a7877f982c34520f
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2014-06-21 13:59:41 +0300

    Alueen testit kuntoon

--- Tiralabra_maven/src/test/java/tietorakenteet/AlueTest.java
+++ Tiralabra_maven/src/test/java/tietorakenteet/AlueTest.java
@@ -1,11 +1,11 @@
-
 package tietorakenteet;
 
+import apurakenteet.Kuvalukija;
+import java.awt.image.BufferedImage;
 import junit.framework.TestCase;
 
 /**
  *
- * @author jsopakar
  */
 public class AlueTest extends TestCase {
     
@@ -33,6 +33,54 @@ public class AlueTest extends TestCase {
         Node n = a.getnode(1, 9);
         
         assertEquals(9, n.getKustannus());
+        
+    }
+    
+    public void testPienenAlueenluonti() {
+        Alue a = new Alue(8);
+        a.luoPieniTestitaulukko();
+        
+        Node n = a.getnode(1, 3);
+        
+        assertEquals(9, n.getKustannus());
+        
+    }
+
+    public void testToimiikoTaulukostaluonti() {
+        Node[][] taulukko = new Node[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Node n = new Node(i,j,0);
+                taulukko[i][j] = n;
+            }
+        }
+        Alue a = new Alue(taulukko, 10, 10);
+        
+        assertEquals(10, a.getKorkeus());
+        assertEquals(10, a.getLeveys());
+    }
+    
+    public void testToimiikoToString() {
+        Alue a = new Alue(8);
+        a.luoPieniTestitaulukko();
+        a.getnode(0, 2).setOnReitilla(true);
+        
+        assertEquals(72, a.toString().length());
+        assertEquals(true, a.toString().startsWith("00-0"));
+        
+    }
+    
+    public void testToimiikoKuvanAsetus() {
+        Alue a = new Alue(8);
+        a.luoPieniTestitaulukko();
+        
+        Kuvalukija kl = new Kuvalukija();
+        kl.muodostaAlue();
+        
+        a.setAlueenKuva(kl.getKuva());
+        BufferedImage kuva = a.getAlueenKuva();
+        assertEquals(80, kuva.getHeight());
+        assertEquals(80, kuva.getWidth());
         
     }
 }
