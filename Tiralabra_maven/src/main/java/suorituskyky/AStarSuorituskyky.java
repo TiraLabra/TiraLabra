0fa20f9da9b6dad0ab6b3a2cb3dbc6f0e787ebf7
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2014-06-22 18:16:40 +0300

    Suorituskykytestausta

--- Tiralabra_maven/src/main/java/suorituskyky/AStarSuorituskyky.java
+++ Tiralabra_maven/src/main/java/suorituskyky/AStarSuorituskyky.java
@@ -1,16 +1,9 @@
 package suorituskyky;
 
-import apurakenteet.Kuvalukija;
-import apurakenteet.Kuvanayttaja;
+import apurakenteet.*;
 import hakualgoritmit.AStar;
-import heuristiikat.Dijkstra;
-import heuristiikat.Euklidinen;
-import heuristiikat.Heuristiikka;
-import heuristiikat.Manhattan;
-import sun.misc.ASCIICaseInsensitiveComparator;
-import tietorakenteet.Alue;
-import tietorakenteet.ArrayListOma;
-import tietorakenteet.Node;
+import heuristiikat.*;
+import tietorakenteet.*;
 
 /**
  * Apuluokka, jonka avulla voi suorittaa suorituskykytestasuksia.
@@ -18,14 +11,15 @@ import tietorakenteet.Node;
  */
 public class AStarSuorituskyky {
 
-    private static String kuvanimi  = "testi.bmp";
+    private static String kuvanimi  = "100x100.bmp";
     private static int alkurivi     = 0;
     private static int alkusarake   = 0;
-    private static int loppurivi    = 218;
-    private static int loppusarake  = 240;
-    //private static Heuristiikka h   = new Dijkstra();
-    private static Heuristiikka h   = new Euklidinen();
+    private static int loppurivi    = 88;
+    private static int loppusarake  = 88;
+    
+    private static Heuristiikka h   = new Dijkstra();
     //private static Heuristiikka h   = new Manhattan();
+    //private static Heuristiikka h   = new Euklidinen();
     
     private AStar as;
     
@@ -65,8 +59,5 @@ public class AStarSuorituskyky {
         }
         
     }
-    
-    
-    
     
 }
