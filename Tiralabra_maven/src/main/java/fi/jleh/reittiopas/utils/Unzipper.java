af9c84cd6d25fa3407c7afa8ca9661a4024a02f0
jleh <jupelehtinen@gmail.com>
2014-06-22 15:35:05 +0300

    Updated zip file handling to work with JAR

--- Tiralabra_maven/src/main/java/fi/jleh/reittiopas/utils/Unzipper.java
+++ Tiralabra_maven/src/main/java/fi/jleh/reittiopas/utils/Unzipper.java
@@ -1,7 +1,12 @@
 package fi.jleh.reittiopas.utils;
 
+import java.io.File;
+import java.net.URL;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * Util class to unzip files.
@@ -9,18 +14,25 @@ import net.lingala.zip4j.exception.ZipException;
  */
 public class Unzipper {
 
+	private static final String source = "/all.zip";
+	private static final String destination = System.getProperty("java.io.tmpdir") + "/j_reittiopas";
+	private static final String jarDestination = destination + "/all.zip";
+	
 	/**
 	 * Unzips timetable data XML file to temp dir.
 	 * @return Path to XML file.
 	 */
 	public static String unzipTimetableData() {
-		String source = "all.zip";
-		String destination = System.getProperty("java.io.tmpdir") + "/j_reittiopas";
-		
 		try {
-			return extract(source, destination);
-		} catch (ZipException e) {
-			try { // I couldn't get Maven to work with tests and JAR file so file paths are handled ugly.
+			// Copy zip file from JAR to temp and unzip
+			URL url = Unzipper.class.getClass().getResource(source);
+			File tmpFile = new File(jarDestination);
+			FileUtils.copyURLToFile(url, tmpFile);
+			
+			return extract(jarDestination, destination);
+		} catch (Exception e) {
+			// Junit tests have no JAR file and zip is extracted in this catch block
+			try {
 				return extract("src/main/resources/all.zip", destination);
 			} catch (Exception e2) {
 				e.printStackTrace();
