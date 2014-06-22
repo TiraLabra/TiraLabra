60616c646219101496536da31774e9318313d8e7
jleh <jupelehtinen@gmail.com>
2014-06-22 16:54:41 +0300

    Performance tests

--- Tiralabra_maven/src/test/java/fi/jleh/reittiopas/routing/RoutingTest.java
+++ Tiralabra_maven/src/test/java/fi/jleh/reittiopas/routing/RoutingTest.java
@@ -1,12 +1,15 @@
 package fi.jleh.reittiopas.routing;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import fi.jleh.reittiopas.exception.RoutingFailureException;
 import fi.jleh.reittiopas.model.Station;
 import fi.jleh.reittiopas.parser.ReittiopasXMLParser;
+import fi.jleh.reittiopas.quadtree.BoundingBox;
 import fi.jleh.reittiopas.router.Router;
 import fi.jleh.reittiopas.router.RouterResult;
 import fi.jleh.reittiopas.utils.DataStructuresDto;
@@ -87,5 +90,30 @@ public class RoutingTest {
 		}
 		
 		System.out.println("Route with time " + time + " best: " + previousBestForRoute);
+	}
+	
+	// Performance tests are here, so we don't have to reload XML
+	
+	@Test
+	public void testQuadtreePerformance() {
+		System.out.println("Quadtree performance test");
+		Station station = dataStructures.getStationMap().get(1113127);
+		BoundingBox box = new BoundingBox(station.getX(), station.getY(), 100);
+		
+		long start = System.currentTimeMillis();
+		for (Station stat : dataStructures.getStationList()) {
+			box.containsPoint(stat);
+		}
+		
+		long time1 = (System.currentTimeMillis() - start);
+		System.out.println("O(n) time: " + time1 + " ms");
+		
+		start = System.currentTimeMillis();
+		dataStructures.getStationSpatial().queryRange(box);
+
+		long time2 = (System.currentTimeMillis() - start);
+		System.out.println("Quadtree time: " + time2 + " ms");
+		
+		System.out.println("Difference " + (time1 - time2) + " ms");
 	}
 }
