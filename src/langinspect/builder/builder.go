7caca6841953c87be9699e598de28a9930688437
Pyry Kontio <pyry.kontio@drasa.eu>
2014-08-05 05:21:53 +0300

    Implementoin builderiin StreamBytes-funktion joka striimaa oppimisdata-kansion bitteinä kokonaisuudessaan.

--- src/langinspect/builder/builder.go
+++ src/langinspect/builder/builder.go
@@ -1,14 +1,62 @@
 package builder
-"Builds a trie-based database of n-gram frequencies in different languages"
+
+/* Builds a trie-based database of n-gram frequencies in different languages */
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/golddranks/TiraLabra/src/trie"
-	"io"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-func Build(input io.Reader) {
+const debug = true
+
+func StreamBytes(dir string) chan byte {
+	byteStream := make(chan byte)
+	go func() {
+		files, _ := ioutil.ReadDir(dir)
+		for _, f := range files {
+			if f.IsDir() {
+				if debug {
+					fmt.Println(f.Name() + " is a directory.")
+				}
+				continue
+			}
+			file, err := os.Open(path.Join(dir, f.Name()))
+			if err != nil {
+				fmt.Println("Couldn't open " + f.Name() + "!")
+				continue
+			}
+			if debug {
+				fmt.Println("Opened " + f.Name())
+			}
+			reader := bufio.NewReader(file)
+
+			for {
+				b, err := reader.ReadByte()
+				if err != nil {
+					if debug {
+						fmt.Println("EOF!")
+					}
+					break
+				}
+				byteStream <- b
+			}
+
+		}
+		close(byteStream)
+	}()
+	return byteStream
+}
+
+func Build(dir string) {
+	byteStream := StreamBytes(dir)
 	dict := trie.CreateNode()
+	for b := range byteStream {
+		fmt.Printf("%c\n", int(b))
+	}
 	dict.Add("jee", "juu")
 	fmt.Println("output")
 }
