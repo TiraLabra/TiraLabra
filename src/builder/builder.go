28da1389ed0da3ed9a02c325dbb5b0d70ac33cc9
Pyry Kontio <pyry.kontio@drasa.eu>
2014-08-05 03:17:46 +0300

    Awesome! Basic trie functionality seems to work.

--- src/builder/builder.go
+++ src/builder/builder.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 	t := trie.CreateNode()
-	o := "JEA"
-	fmt.Println(t)
-	t.Add("hajoo", o)
-	fmt.Println(t)
+	t.Add("hajoo", "HAJOO")
+	t.Add("aakkos", "AAKKOS")
+	t.Add("aakkosia", "AAKKOSIA")
+	t.Add("haju", "HAJU")
+	t.Add("soppaako", "SOPPAAKO")
+	fmt.Println(t.Get("hajoo"))
+	fmt.Println(t.Get("aakkos"))
+	fmt.Println(t.Get("aakkosia"))
+	fmt.Println(t.Get("haju"))
+	fmt.Println(t.Get("soppaako"))
 }
--- src/trie/trie.go
+++ src/trie/trie.go
@@ -7,14 +7,21 @@ type Node struct {
 	Value    interface{}
 }
 
+const debug = false
+
 func CreateNode() *Node {
+	if debug {
+		fmt.Println("Create node")
+	}
 	n := &Node{}
 	n.Children = [256]*Node{}
 	return n
 }
 
 func (n *Node) Add(key string, object interface{}) {
-	fmt.Println("Add start " + key)
+	if debug {
+		fmt.Println("Add with key: " + key)
+	}
 	if len(key) > 0 {
 		child := &n.Children[key[0]]
 		if *child == nil {
@@ -23,13 +30,20 @@ func (n *Node) Add(key string, object interface{}) {
 		(*child).Add(key[1:], object)
 	} else {
 		n.Value = object
+		if debug {
+			fmt.Println("Added object: ", object)
+		}
 	}
 }
 
 func (n *Node) Get(key string) interface{} {
 	if len(key) > 0 {
 		child := n.Children[key[0]]
-		return child.Get(key[1:])
+		if child == nil {
+			return nil
+		} else {
+			return child.Get(key[1:])
+		}
 	} else {
 		return n.Value
 	}
