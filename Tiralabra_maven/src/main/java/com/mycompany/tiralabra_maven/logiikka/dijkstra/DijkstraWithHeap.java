f4a15663c0c10d42d656d2db67806ed6aff2b7e2
HannuPekkaHonkanen <hannu.p.honkanen@helsinki.fi>
2014-08-09 19:26:56 +0300

    DijkstraWithHeap siistitty jakamalla pienempiin metodeihin

--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/logiikka/dijkstra/DijkstraWithHeap.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/logiikka/dijkstra/DijkstraWithHeap.java
@@ -13,105 +13,43 @@ import java.util.Stack;
  */
 public class DijkstraWithHeap {
 
-    private Paikka[][] paikat;
     private Piste lahtoPiste;
     private Piste maaliPiste;
-//    private int n;
-//    private int m;
+    private int[][] kartta;
+    private Paikka[][] paikat;
 
     public DijkstraWithHeap(int[][] kartta, Piste lahtoPiste, Piste maaliPiste) {
-//        this.n = ;
-//        this.m = ;
 
         this.lahtoPiste = lahtoPiste;
         this.maaliPiste = maaliPiste;
+        this.kartta = kartta;
         this.paikat = new Paikka[kartta.length][kartta[0].length];
-
-        // InitialiseSingleSource
-        for (int i = 0; i < this.paikat.length; i++) {
-            for (int j = 0; j < this.paikat[0].length; j++) {
-                // luokan Paikka konstruktori asettaa Paikan julkisen muuttujan etaisyysAlkuun arvoksi noin aareton
-                this.paikat[i][j] = new Paikka(i, j, kartta[i][j]);
-//                this.paikat[i][j] = new Paikka(vuori[i][j]);
-            }
-        }
-        this.paikat[lahtoPiste.x][lahtoPiste.y].etaisyysAlkuun = 0;
 
     }
 
     public int ratkaise() {
         // Dijkstra with min-heap
 
+        initialiseSingleSource();
 
-        int iVierus;
-        int jVierus;
+        this.paikat[lahtoPiste.x][lahtoPiste.y].etaisyysAlkuun = 0;
 
-//        System.out.println("n m: " + n + " " + m);
-
-
-        JavaKeko<Paikka> heap = new JavaKeko<Paikka>();
-//        PriorityQueue<Paikka> kekoPieninEtaisyys = new PriorityQueue<Paikka>(n*m);
-
-        Paikka paikkaU;
-
-//        boolean muuttuikoV;
-
-
-        for (int i = 0; i < this.paikat.length; i++) {
-            for (int j = 0; j < this.paikat[0].length; j++) {
-                heap.heapInsert(this.paikat[i][j]);
-
-                iVierus = i;
-                jVierus = j - 1;
-                if (iVierus >= 0 && iVierus < this.paikat.length && jVierus >= 0 && jVierus < this.paikat[0].length) {
-                    this.paikat[i][j].vierusPaikat.add(this.paikat[iVierus][jVierus]);
-                    System.out.println("i, j, iVierus, jVierus: " + i + j + " " + iVierus + jVierus);
-                }
-                iVierus = i;
-                jVierus = j + 1;
-                if (iVierus >= 0 && iVierus < this.paikat.length && jVierus >= 0 && jVierus < this.paikat[0].length) {
-                    this.paikat[i][j].vierusPaikat.add(this.paikat[iVierus][jVierus]);
-                    System.out.println("i, j, iVierus, jVierus: " + i + j + " " + iVierus + jVierus);
-                }
-                iVierus = i - 1;
-                jVierus = j;
-                if (iVierus >= 0 && iVierus < this.paikat.length && jVierus >= 0 && jVierus < this.paikat[0].length) {
-                    this.paikat[i][j].vierusPaikat.add(this.paikat[iVierus][jVierus]);
-                    System.out.println("i, j, iVierus, jVierus: " + i + j + " " + iVierus + jVierus);
-                }
-                iVierus = i + 1;
-                jVierus = j;
-                if (iVierus >= 0 && iVierus < this.paikat.length && jVierus >= 0 && jVierus < this.paikat[0].length) {
-                    this.paikat[i][j].vierusPaikat.add(this.paikat[iVierus][jVierus]);
-                    System.out.println("i, j, iVierus, jVierus: " + i + j + " " + iVierus + jVierus);
-                }
-            }
-        }
-
-
-
+        JavaKeko<Paikka> heap = rakennaKekoJaAsetaVieruspaikat();
 
         System.out.println(this.paikat[0][0].vierusPaikat);
 
-//        for (int i = 0; i < this.paikat.length; i++) {
-//            for (int j = 0; j < this.paikat[0].length; j++) {
-//                System.out.println(kekoPieninEtaisyys.poll().etaisyys);
-//            }
-//        }
-
         System.out.println("");
+
+        Paikka paikkaU;
 
         while (!heap.heapIsEmpty()) {
             paikkaU = heap.heapDelMin();
             System.out.println("paikkaU " + paikkaU.i + paikkaU.j + " " + paikkaU.etaisyysAlkuun);
-//            paikkaU.kay();
             for (Paikka paikkaV : paikkaU.vierusPaikat) {
                 System.out.println("paikkaV " + paikkaV.i + paikkaV.j + " " + paikkaV.etaisyysAlkuun);
-//                muuttuikoV = false;
                 if (relax(paikkaU, paikkaV)) {
                     System.out.println("V muuttui");
-                    heap.heapDelete(paikkaV);
-                    heap.heapInsert(paikkaV);
+                    heap.heapDecreaseKey(paikkaV);
                 }
             }
         }
@@ -128,7 +66,44 @@ public class DijkstraWithHeap {
         return this.paikat[maaliPiste.x][maaliPiste.y].etaisyysAlkuun;
     }
 
-    public boolean relax(Paikka paikkaU, Paikka paikkaV) {
+    private void initialiseSingleSource() {
+        for (int i = 0; i < this.paikat.length; i++) {
+            for (int j = 0; j < this.paikat[0].length; j++) {
+                // luokan Paikka konstruktori asettaa Paikan julkisen muuttujan etaisyysAlkuun arvoksi noin aareton
+                this.paikat[i][j] = new Paikka(i, j, this.kartta[i][j]);
+            }
+        }
+    }
+
+    private JavaKeko<Paikka> rakennaKekoJaAsetaVieruspaikat() {
+        JavaKeko<Paikka> heap = new JavaKeko<Paikka>();
+
+        for (int i = 0; i < this.paikat.length; i++) {
+            for (int j = 0; j < this.paikat[0].length; j++) {
+
+                heap.heapInsert(this.paikat[i][j]);
+
+                asetaVieruspaikka(this.paikat[i][j], i, j - 1);
+                asetaVieruspaikka(this.paikat[i][j], i, j + 1);
+                asetaVieruspaikka(this.paikat[i][j], i - 1, j);
+                asetaVieruspaikka(this.paikat[i][j], i + 1, j);
+
+            }
+        }
+
+        return heap;
+
+    }
+
+    private void asetaVieruspaikka(Paikka paikka, int iVierus, int jVierus) {
+        if (iVierus >= 0 && iVierus < this.paikat.length && jVierus >= 0 && jVierus < this.paikat[0].length) {
+            paikka.vierusPaikat.add(this.paikat[iVierus][jVierus]);
+            System.out.println("i, j, iVierus, jVierus: " + paikka.i + paikka.j + " " + iVierus + jVierus);
+        }
+
+    }
+
+    private boolean relax(Paikka paikkaU, Paikka paikkaV) {
         if (paikkaV.etaisyysAlkuun > paikkaU.etaisyysAlkuun + Math.abs(paikkaV.aikaKustannus)) {
             paikkaV.etaisyysAlkuun = paikkaU.etaisyysAlkuun + Math.abs(paikkaV.aikaKustannus);
             paikkaV.polku = paikkaU;
@@ -141,28 +116,19 @@ public class DijkstraWithHeap {
 
         Stack<Paikka> pino = new Stack<Paikka>();
 
-        Paikka u = this.paikat[maaliPiste.x][maaliPiste.y].polku;
+        Paikka paikkaU = this.paikat[maaliPiste.x][maaliPiste.y].polku;
 
-        while (!u.equals(this.paikat[this.lahtoPiste.x][this.lahtoPiste.y])) {
-            pino.push(u);
-            u = u.polku;
+        while (!paikkaU.equals(this.paikat[this.lahtoPiste.x][this.lahtoPiste.y])) {
+            pino.push(paikkaU);
+            paikkaU = paikkaU.polku;
         }
 
         System.out.println("polku");
 
         while (!pino.empty()) {
-            u=pino.pop();
-            System.out.print(u.i);
-            System.out.print(", ");
-            System.out.print(u.j);
-            System.out.println();
-            
+            paikkaU = pino.pop();
+            System.out.println(paikkaU.i + ", " + paikkaU.j);
         }
-
-
-
-
-
 
     }
 }
