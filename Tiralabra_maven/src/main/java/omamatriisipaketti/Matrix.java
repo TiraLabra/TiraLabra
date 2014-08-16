11f432358cdf8b236856720398d20ec21293af2e
risto <risto@localhost.home>
2014-08-16 17:06:01 +0300

    matriisien ja laskutoimitusten omat toteutukset

---
+++ Tiralabra_maven/src/main/java/omamatriisipaketti/Matrix.java
@@ -0,0 +1,17 @@
+package omamatriisipaketti;
+
+
+public class Matrix {
+    /**
+     * Matriisit annetaan taulukkoina, matriisi-olio vastaa sopivan tallennusmuodon valinnasta
+     */
+    double[][] taulukkoesitys;
+    
+    
+    public Matrix(double[][] arvot) {
+        this.taulukkoesitys = arvot;
+    }
+    
+    
+    
+}
