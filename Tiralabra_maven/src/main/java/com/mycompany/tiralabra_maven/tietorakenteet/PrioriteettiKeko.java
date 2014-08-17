ed72c7429c69717eceaba07576909165400f1983
Mikko Kumara <mikko.kumara@gmail.com>
2014-08-17 23:23:34 +0300

    parempi prioriteettikeko ja testeja

--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/tietorakenteet/PrioriteettiKeko.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/tietorakenteet/PrioriteettiKeko.java
@@ -5,48 +5,52 @@
  */
 package com.mycompany.tiralabra_maven.tietorakenteet;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
+import java.util.Comparator;
 
 /**
  *
  * @author mikko
+ * @param <E>
  */
 public class PrioriteettiKeko<E> {
 
-    //private Object[] taulukko;
-    private int[] taulukko;
+    private Object[] taulukko;
     private int koko;
-    private int temp;
-    
-    
+    private Object temp;
+
+    private final Comparator<? super E> vertailija;
+
     public PrioriteettiKeko() {
-        //his.taulukko = new Object[100];
-        this.taulukko = new int[100];
+        this(null);
     }
-    
-    public int seuraava() {
+
+    public PrioriteettiKeko(Comparator<? super E> vertailija) {
+        this.vertailija = vertailija;
+        this.taulukko = new Object[100];
+    }
+
+    public E seuraava() {
         if (koko == 0) {
-            return -1;
+            return null;
         }
-        int max = taulukko[0];
-        taulukko[0] = taulukko[koko-1];
+        E max = (E) (taulukko[0]);
+        taulukko[0] = taulukko[koko - 1];
         koko--;
         heapify(0);
         return max;
     }
-    
-    public void lisaa(int luku) {
+
+    public void lisaa(E lisattava) {
         koko++;
-        int i = koko-1;
-        while (i > 0 && luku > taulukko[vanhempi(i)]) {
-            taulukko[i] = taulukko[vanhempi(i)];
+        int i = koko - 1;
+        taulukko[i] = lisattava;
+
+        while (i > 0 && (suurempi(i, vanhempi(i)) == i)) {
+            swap(i, vanhempi(i));
             i = vanhempi(i);
         }
-        taulukko[i] = luku;
+        taulukko[i] = lisattava;
     }
-
 
     private int vanhempi(int indeksi) {
         return indeksi / 2;
@@ -59,28 +63,44 @@ public class PrioriteettiKeko<E> {
     private int oikeaLapsi(int indeksi) {
         return 2 * indeksi + 1;
     }
-    
+
+    private void swap(int i, int j) {
+        this.temp = taulukko[i];
+        taulukko[i] = taulukko[j];
+        taulukko[j] = this.temp;
+    }
+
     private void heapify(int i) {
+
         int right = oikeaLapsi(i);
         int left = vasenLapsi(i);
         if (right <= koko) {
-            int largest;
-            if (taulukko[left] > taulukko[right]) {
-                largest = left;
-            } else {
-                largest = right;
-            }
-            if (taulukko[i] < taulukko[largest]) {
-                temp = taulukko[i];
-                taulukko[i] = taulukko[largest];
-                taulukko[largest] = temp;
+            int largest = suurempi(left, right);
+
+            if (suurempi(largest, i) == largest) {
+                swap(i, largest);
                 heapify(largest);
-            } 
-        } else if (left == koko && taulukko[i] < taulukko[left]) {
-            temp = taulukko[i];
-            taulukko[i] = taulukko[left];
-            taulukko[left] = temp;
+            }
+
+        } else if (left == koko && (suurempi(left, i) == left)) {
+            swap(i, left);
         }
     }
 
+    private int suurempi(int i, int j) {
+        if (vertailija == null) {
+            if (((Comparable<? super E>) (taulukko[i])).compareTo((E) (taulukko[j])) < 0) {
+                return i;
+            } else {
+                return j;
+            }
+        } else {
+
+            if (vertailija.compare((E) taulukko[i], (E) taulukko[j]) < 0) {
+                return i;
+            } else {
+                return j;
+            }
+        }
+    }
 }
---
+++ Tiralabra_maven/src/test/java/com/mycompany/tiralabra_maven/algoritmi/SimulaatioTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tiralabra_maven.algoritmi;
+
+import com.mycompany.tiralabra_maven.Koordinaatit;
+import static junit.framework.Assert.*;
+import org.junit.*;
+
+/**
+ *
+ * @author mikko
+ */
+public class SimulaatioTest {
+
+    private Simulaatio simulaatio;
+
+    public SimulaatioTest() {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.simulaatio = new Simulaatio(false);
+    }
+    
+    
+
+
+}
