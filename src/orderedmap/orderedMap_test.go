65f8f0f080b3277a6eeec7a9b4137db99bf0dd3b
Pyry Kontio <pyry.kontio@drasa.eu>
2014-08-25 04:52:17 +0300

    First perf test done. Performance seems to be hideous???

--- src/orderedmap/orderedMap_test.go
+++ src/orderedmap/orderedMap_test.go
@@ -4,54 +4,73 @@ import (
 	"fmt"
 	"github.com/golddranks/TiraLabra/src/orderedmap/darraytrie"
 	"github.com/golddranks/TiraLabra/src/orderedmap/naivetrie"
+	"math/rand"
 	"testing"
 )
 
 var darray darraytrie.Trie
 var naive naivetrie.Node
 
-var flagtestsA = []struct {
-	in  string
+type perftestType struct {
+	in  []byte
 	out int
-}{
-	{"hajoo", 1},
-	{"haju", 2},
-	{"aakkos", 3},
-	{"aakkosia", 4},
-	{"soppaako", 5},
 }
 
-func TestBasicsDarray(t *testing.T) {
-	darray := darraytrie.NewTrie(10000, 256)
-	for _, test := range flagtestsA {
-		darray.Add([]byte(test.in), test.out)
+var perftest = []perftestType{}
+
+func randoms() perftestType {
+	b := make([]byte, 9)
+	for i := 0; i < 9; i++ {
+		b[i] = byte(rand.Intn(256))
 	}
-	for _, test := range flagtestsA {
-		iter := darray.TryAndGet([]byte(test.in))
-		if iter.Value() == 0 {
-			fmt.Println("Output is 0 and it definitely shouldn't be!")
-			t.Fail()
-		} else if iter.Value() != test.out {
-			fmt.Println("Should have returned " + string(test.out) + " but returned " + string(iter.Value()))
-			t.Fail()
+	c := rand.Int()
+	return perftestType{b, c}
+}
+
+func prepareInput(n int) {
+	for i := 0; i < n; i++ {
+		perftest = append(perftest, randoms())
+	}
+}
+
+func BenchmarkBasicsDarray(t *testing.B) {
+	darray := darraytrie.NewTrie(10000, 256)
+	prepareInput(1000)
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		for _, test := range perftest {
+			darray.Add(test.in, test.out)
+		}
+		for _, test := range perftest {
+			iter := darray.TryAndGet(test.in)
+			if iter.Value() == 0 {
+				fmt.Println("Output is 0 and it definitely shouldn't be!")
+				t.Fail()
+			} else if iter.Value() != test.out {
+				fmt.Println("Should have returned " + string(test.out) + " but returned " + string(iter.Value()))
+				t.Fail()
+			}
 		}
 	}
 	fmt.Println("TEST A for DARRAY TRIE PASSES.\n\n\n")
 }
 
-func TestBasicsNaive(t *testing.T) {
+func BenchmarkBasicsNaive(t *testing.B) {
 	naive := naivetrie.NewNode()
-	for _, test := range flagtestsA {
-		naive.Add([]byte(test.in), test.out)
-	}
-	for _, test := range flagtestsA {
-		val := naive.TryAndGet([]byte(test.in)).(int)
-		if val == 0 {
-			fmt.Println("Output is 0 and it definitely shouldn't be!")
-			t.Fail()
-		} else if val != test.out {
-			fmt.Println("Should have returned " + string(test.out) + " but returned " + string(val))
-			t.Fail()
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		for _, test := range perftest {
+			naive.Add(test.in, test.out)
+		}
+		for _, test := range perftest {
+			val := naive.TryAndGet(test.in).(int)
+			if val == 0 {
+				fmt.Println("Output is 0 and it definitely shouldn't be!")
+				t.Fail()
+			} else if val != test.out {
+				fmt.Println("Should have returned " + string(test.out) + " but returned " + string(val))
+				t.Fail()
+			}
 		}
 	}
 	fmt.Println("TEST A for NAIVE TRIE PASSES.\n\n\n")
