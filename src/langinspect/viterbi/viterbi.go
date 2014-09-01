aa2eaf4642b2c0ac06e657cf8a981cf2ac9ba7cc
Pyry Kontio <pyry.kontio@drasa.eu>
2014-09-01 03:39:31 +0300

    Viterbi algorithm seems to work now. Tests pass.

--- src/langinspect/viterbi/viterbi.go
+++ src/langinspect/viterbi/viterbi.go
@@ -1,31 +1,27 @@
 package viterbi
 
-import (
-	"github.com/golddranks/TiraLabra/src/orderedmap/naivetrie"
-)
+func viterbi(obs [][]byte, states int, trans_prob [][]float64, start_prob []float64, emit_prob func([]byte, int) float64) ([][]float64, [][]int) {
 
-type LangTable []int
+	probs := [][]float64{make([]float64, states)}
+	paths := make([][]int, states)
 
-func viterbi(obs [][]byte, states []int, trans_prob [][]float64, start_prob []float64, emit_prob *naivetrie.Node) ([][]float64, [][]int) {
-
-	numStates := len(states)
-	probs := [][]float64{make([]float64, numStates)}
-	paths := [][]int{}
-
-	for s := range states {
-		var emit_p float64 = float64(emit_prob.TryAndGet(obs[0]).(LangTable)[s+1]) / float64(emit_prob.TryAndGet(obs[0]).(LangTable)[0])
+	for s := 0; s < states; s++ {
+		var emit_p float64 = emit_prob(obs[0], s)
 		probs[0][s] = start_prob[s] * emit_p
+		paths[s] = []int{s}
 	}
 
-	for o := range obs[1:] {
-		probs = append(probs, make([]float64, numStates))
-		newpaths := make([][]int, len(states))
-		for nowState := range states {
-			var emit_p float64 = float64(emit_prob.TryAndGet(obs[o]).(LangTable)[nowState+1]) / float64(emit_prob.TryAndGet(obs[o]).(LangTable)[0])
+	for o := 1; o < len(obs); o++ {
+		probs = append(probs, make([]float64, states))
+		newpaths := make([][]int, states)
+		for nowState := 0; nowState < states; nowState++ {
+			var nowStateEmitProb float64 = emit_prob(obs[o], nowState)
 			var max_prob float64
 			var max_lastState int
-			for lastState := range states {
-				thisProb := probs[o-1][lastState] * trans_prob[lastState][nowState] * emit_p
+			for lastState := 0; lastState < states; lastState++ {
+				lastStateProb := probs[o-1][lastState]
+				transitionProb := trans_prob[lastState][nowState]
+				thisProb := lastStateProb * transitionProb * nowStateEmitProb
 				if max_prob < thisProb {
 					max_prob = thisProb
 					max_lastState = lastState
--- src/langinspect/viterbi/viterbi_test.go
+++ src/langinspect/viterbi/viterbi_test.go
@@ -2,58 +2,90 @@ package viterbi
 
 import (
 	"fmt"
-	"github.com/golddranks/TiraLabra/src/orderedmap/naivetrie"
 	"math"
 	"testing"
 )
+
+type LangTable []int
+
+func emit_probA(obs []byte, state int) float64 {
+	switch obs[0] {
+	case byte('a'):
+		return []float64{0.33333, 0.16666}[state]
+	case byte('b'):
+		return []float64{0.16666, 0.33333}[state]
+	}
+	return 0
+}
 
 func TestA(t *testing.T) {
 	fmt.Println("Test A")
 	a := []byte("a")
 	observations := [][]byte{a}
-	states := []int{0, 1}
+	states := 2
 	start_prob := []float64{0.5, 0.5}
-	trans_prob := [][]float64{[]float64{0.5, 0.5}, []float64{0.5, 0.5}}
-	dict := naivetrie.NewNode()
-	dict.Add([]byte("a"), LangTable{30, 10, 20})
-	dict.Add([]byte("b"), LangTable{30, 20, 10})
-	result := viterbi(observations, states, trans_prob, start_prob, dict)
+	trans_prob := [][]float64{
+		[]float64{0.5, 0.5},
+		[]float64{0.5, 0.5},
+	}
+	probs, paths := viterbi(observations, states, trans_prob, start_prob, emit_probA)
 
-	expected := [][]float64{[]float64{0.16666, 0.33333}}
+	expected := [][]float64{[]float64{0.16666, 0.08333}}
 
 	for i := range expected {
 		for s := range expected[i] {
-			if math.Abs(result[i][s]-expected[i][s]) > 0.00001 {
+			if math.Abs(probs[i][s]-expected[i][s]) > 0.00001 {
 				t.Fail()
-				fmt.Println("Result[", i, "][", s, "] should be around", expected[i][s], "but was", result[i][s])
+				fmt.Println("Probs[", i, "][", s, "] should be around", expected[i][s], "but was", probs[i][s])
 			}
 		}
 	}
+	fmt.Println(paths)
 
+}
+
+func emit_probB(obs []byte, state int) float64 {
+	switch obs[0] {
+	case byte('n'):
+		return []float64{0.5, 0.1}[state]
+	case byte('c'):
+		return []float64{0.4, 0.3}[state]
+	case byte('d'):
+		return []float64{0.1, 0.6}[state]
+	}
+	return 0
 }
 
 func TestB(t *testing.T) {
 	fmt.Println("Test B")
-	a := []byte("a")
-	b := []byte("b")
-	observations := [][]byte{a, b, b, a, a, b, a, b, b, b}
-	states := []int{0, 1}
-	start_prob := []float64{0.5, 0.5}
-	trans_prob := [][]float64{[]float64{0.5, 0.5}, []float64{0.5, 0.5}}
-	dict := naivetrie.NewNode()
-	dict.Add([]byte("a"), LangTable{30, 10, 20})
-	dict.Add([]byte("b"), LangTable{30, 20, 10})
-	result := viterbi(observations, states, trans_prob, start_prob, dict)
+	normal := []byte("n")
+	cold := []byte("c")
+	dizzy := []byte("d")
+	observations := [][]byte{normal, cold, dizzy}
+	states := 2
+	start_prob := []float64{0.6, 0.4}
+	trans_prob := [][]float64{
+		[]float64{0.7, 0.3},
+		[]float64{0.4, 0.6},
+	}
+	probs, paths := viterbi(observations, states, trans_prob, start_prob, emit_probB)
 
-	expected := [][]float64{[]float64{0.16666, 0.33333}}
+	expected := [][]float64{
+		[]float64{0.3, 0.04},
+		[]float64{0.084, 0.027},
+		[]float64{0.00588, 0.01512},
+	}
 
 	for i := range expected {
 		for s := range expected[i] {
-			if math.Abs(result[i][s]-expected[i][s]) > 0.00001 {
+			if math.Abs(probs[i][s]-expected[i][s]) > 0.00001 {
 				t.Fail()
-				fmt.Println("Result[", i, "][", s, "] should be around", expected[i][s], "but was", result[i][s])
+				fmt.Println("Probs[", i, "][", s, "] should be around", expected[i][s], "but was", probs[i][s])
 			}
 		}
 	}
+	fmt.Println(observations)
+	fmt.Println(probs)
+	fmt.Println(paths)
 
 }
