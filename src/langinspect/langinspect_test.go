a30b2b69630f4e9b4a0cd107ce605d6f7ac90e8c
Pyry Kontio <pyry.kontio@drasa.eu>
2014-09-03 15:51:40 +0300

    Tests pass.

--- src/langinspect/langinspect_test.go
+++ src/langinspect/langinspect_test.go
@@ -15,6 +15,20 @@ func printLangTable(db *naivetrie.Node, ngram string) {
 	fmt.Println()
 }
 
+func hasErrors(path []int, expected_path []int, text []byte) bool {
+	for i, p := range path {
+		if expected_path[i] != p {
+			fmt.Println("Got:")
+			printPath(text, path)
+			fmt.Println("Should be:")
+			printPath(text, expected_path)
+			fmt.Println()
+			return true
+		}
+	}
+	return false
+}
+
 func TestInspect(t *testing.T) {
 	db := builder.Build("testdata")
 	states := builder.AmountOfLangs // TODO Ugly, ugly global value. Need to refactor.
@@ -31,28 +45,29 @@ func TestInspect(t *testing.T) {
 	text = []byte("joopa joo")
 	path = viterbi.Run(slice(text), states, trans_prob, start_prob, emit_prob)
 	expected_path = []int{1, 1, 1, 1, 1, 1, 1, 1, 1}
-	for i, p := range path {
-		if expected_path[i] != p {
-			printPath(text, path)
-			t.Fail()
-			break
-		}
+	if hasErrors(path, expected_path, text) {
+		t.Fail()
 	}
 
-	start_prob = []float64{0.33333, 0.33333, 0.33333}
-	trans_prob = [][]float64{
-		[]float64{0.96, 0.02, 0.02},
-		[]float64{0.02, 0.96, 0.02},
-		[]float64{0.02, 0.02, 0.96},
-	}
-	text = []byte("jack")
+	text = []byte("dumb")
 	path = viterbi.Run(slice(text), states, trans_prob, start_prob, emit_prob)
 	expected_path = []int{0, 0, 0, 0}
-	for i, p := range path {
-		if expected_path[i] != p {
-			printPath(text, path)
-			t.Fail()
-			break
-		}
+	if hasErrors(path, expected_path, text) {
+		t.Fail()
 	}
+
+	text = []byte("jack")
+	path = viterbi.Run(slice(text), states, trans_prob, start_prob, emit_prob)
+	expected_path = []int{1, 1, 1, 1}
+	if hasErrors(path, expected_path, text) {
+		t.Fail()
+	}
+
+	text = []byte("jac")
+	path = viterbi.Run(slice(text), states, trans_prob, start_prob, emit_prob)
+	expected_path = []int{0, 0, 0, 0}
+	if hasErrors(path, expected_path, text) {
+		t.Fail()
+	}
+
 }
