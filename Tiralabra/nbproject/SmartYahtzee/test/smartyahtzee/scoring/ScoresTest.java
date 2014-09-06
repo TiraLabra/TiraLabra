8fcc468dd072657d9bdb82848eb6f3bb55775b09
unknown <essi.salmenkivi@helsinki.fi>
2014-09-06 17:17:04 +0300

    added some tests

--- Tiralabra/nbproject/SmartYahtzee/test/smartyahtzee/scoring/ScoresTest.java
+++ Tiralabra/nbproject/SmartYahtzee/test/smartyahtzee/scoring/ScoresTest.java
@@ -41,27 +41,37 @@ public class ScoresTest {
     /**
      * Test of calculateBestScore method, of class Scores.
      */
-    @Test
-    public void testCalculateBestScore() {
-        System.out.println("calculateBestScore");
-        int[] dice = {6, 6, 4, 4, 1};
-        boolean[] marked = new boolean[17];
-        marked[15] = true;      //not counting chance or pair
-        marked[8] = true;
-        double expResult = (20/22) * 3.78259;
-        double result = Scores.calculateBestScore(dice, marked);
-        assertEquals(expResult, result, 0.1);
-    }
+//    @Test
+//    public void testCalculateBestScore() {
+//        System.out.println("calculateBestScore");
+//        int[] dice = {6, 6, 4, 4, 1};
+//        boolean[] marked = new boolean[17];
+//        marked[15] = true;      //not counting chance or pair
+//        marked[8] = true;
+//        double expResult = (20/22) * 3.78259;
+//        double result = Scores.calculateBestScore(dice, marked);
+//        assertEquals(expResult, result, 0.1);
+//    }
 
     /**
      * Test of calculateScore method, of class Scores.
      */
     @Test
-    public void testCalculateScore() {
+    public void testSmallStraightScore() {
         System.out.println("calculateScore");
         int index = 14;
-        int[] dice = {5, 5, 5, 2, 2};
-        int expResult = 5*3 + 2*2;
+        int[] dice = {5, 4, 3, 2, 1};
+        int expResult = 15;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testLargeStraightScore() {
+        System.out.println("calculateScore");
+        int index = 14;
+        int[] dice = {6, 5, 4, 3, 2};
+        int expResult = 15;
         int result = Scores.calculateScore(index, dice);
         assertEquals(expResult, result);
     }
@@ -73,6 +83,62 @@ public class ScoresTest {
         int index = 9;
         int[] dice = {6, 6, 4, 4, 1};
         int expResult = 6*2 + 4*2;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    
+    @Test
+    public void testFullHouseScore()
+    {
+        System.out.println("fullHouseScore");
+        int index = 14;
+        int[] dice = {6, 6, 4, 4, 4};
+        int expResult = 6*2 + 4*3;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testTwoPairsScore2()                //testaa että viisi samaa ei kelpaa täyskädeksi
+    {
+        System.out.println("fullHouseScore2");
+        int index = 14;
+        int[] dice = {4, 4, 4, 4, 4};
+        int expResult = 6*2 + 4*2;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testPairScore()
+    {
+        System.out.println("twoPairsScore");
+        int index = 8;
+        int[] dice = {6, 6, 4, 4, 1};
+        int expResult = 6*2;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testThreeOfAKindScore()
+    {
+        System.out.println("twoPairsScore");
+        int index = 10;
+        int[] dice = {6, 6, 6, 4, 1};
+        int expResult = 6*3;
+        int result = Scores.calculateScore(index, dice);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testFourOfAKindScore()
+    {
+        System.out.println("twoPairsScore");
+        int index = 11;
+        int[] dice = {4, 4, 4, 4, 1};
+        int expResult = 4*4;
         int result = Scores.calculateScore(index, dice);
         assertEquals(expResult, result);
     }
