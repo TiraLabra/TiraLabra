ca34ff2898bd36dc9ac3bca5ae7ef21acd419477
ugab <ylinenjaakko@gmail.com>
2014-10-13 11:03:12 +0300

    asd2

--- Tiralabra/test/Käyttöliittymä/KuvaTest.java
+++ Tiralabra/test/Käyttöliittymä/KuvaTest.java
@@ -9,6 +9,10 @@ package Käyttöliittymä;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.imageio.ImageIO;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -22,11 +26,15 @@ import static org.junit.Assert.*;
  */
 public class KuvaTest {
     
+    static Kuva testi;
+    
+    
     public KuvaTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        testi=new Kuva();
     }
     
     @AfterClass
@@ -45,41 +53,12 @@ public class KuvaTest {
      * Test of haeKuva method, of class Kuva.
      */
     @Test
-    public void testHaeKuva() {
-        System.out.println("haeKuva");
-        Kuva instance = new Kuva();
-        instance.haeKuva();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testHaeKuva() throws URISyntaxException, IOException {
+        
+        assertEquals(testi.kuva, ImageIO.read(new File(getClass().getResource("Untitled.jpg").toURI())));
     }
 
-    /**
-     * Test of haeLahto method, of class Kuva.
-     */
-    @Test
-    public void testHaeLahto() {
-        System.out.println("haeLahto");
-        Kuva instance = new Kuva();
-        Point expResult = null;
-        Point result = instance.haeLahto();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of haeMaali method, of class Kuva.
-     */
-    @Test
-    public void testHaeMaali() {
-        System.out.println("haeMaali");
-        Kuva instance = new Kuva();
-        Point expResult = null;
-        Point result = instance.haeMaali();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of paintComponent method, of class Kuva.
@@ -120,69 +99,4 @@ public class KuvaTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of mouseClicked method, of class Kuva.
-     */
-    @Test
-    public void testMouseClicked() {
-        System.out.println("mouseClicked");
-        MouseEvent me = null;
-        Kuva instance = new Kuva();
-        instance.mouseClicked(me);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of mouseEntered method, of class Kuva.
-     */
-    @Test
-    public void testMouseEntered() {
-        System.out.println("mouseEntered");
-        MouseEvent me = null;
-        Kuva instance = new Kuva();
-        instance.mouseEntered(me);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of mouseExited method, of class Kuva.
-     */
-    @Test
-    public void testMouseExited() {
-        System.out.println("mouseExited");
-        MouseEvent me = null;
-        Kuva instance = new Kuva();
-        instance.mouseExited(me);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of mouseDragged method, of class Kuva.
-     */
-    @Test
-    public void testMouseDragged() {
-        System.out.println("mouseDragged");
-        MouseEvent me = null;
-        Kuva instance = new Kuva();
-        instance.mouseDragged(me);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of mouseMoved method, of class Kuva.
-     */
-    @Test
-    public void testMouseMoved() {
-        System.out.println("mouseMoved");
-        MouseEvent me = null;
-        Kuva instance = new Kuva();
-        instance.mouseMoved(me);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
