94710d52a425aef2aa69365c470796e93d6dff5d
appD-eino <eino-oskari.kiiski@helsinki.fi>
2014-11-08 16:04:12 +0200

    Käsi-luokka_päivitetty

--- Tiralabra_maven/src/main/java/com/mycompany/logiikka/Kasi.java
+++ Tiralabra_maven/src/main/java/com/mycompany/logiikka/Kasi.java
@@ -2,30 +2,85 @@ package com.mycompany.logiikka;
 
 /**
  * Luokka kuvaa pelin kättä. Mahdollisia vaihtoehtoja ovat 
- * KIVI, PAPERI, SAKSET, LISKO JA SPOCK
+ * KIVI, PAPERI, SAKSET, LISKO JA SPOCK.
+ * Luokka tietää mitkä kädet voittavat tämä instanssin
  */
 public class Kasi {
+
     private String nimi;
+    private Kasi tamaVoittaaYksi;
+    private Kasi tamaVoittaaKaksi;
+
+    /**
+     * Konstruktori asettaa luokkamuuttujien arvot. 
+     * 
+     * @param nimi Käden nimi 
+     * @param voittaa1 Ensimmäinen käsi joka voittaa tämän käden
+     * @param voittaa2 Toinen käsi, joka voittaa tämän käden
+     */
+    public Kasi(String nimi, Kasi voittaa1, Kasi voittaa2) {
+        this.nimi = nimi;
+        this.tamaVoittaaYksi = voittaa1;
+        this.tamaVoittaaKaksi = voittaa2;
+    }
     
     /**
-     * Konstruktori asettaa kädelle nimen. Nimen tulee olla 
-     * joku seuraavista:
-     * KIVI, PAPERI, SAKSET, LISKO JA SPOCK
+     * Konstruktori asettaa kädelle vain nimen. Lisää voittavat kädet metodien
+     * kautta, jotta peli toimii.
      * 
-     * Konstruktori ei tee virheentarkistusta
-     * 
-     * @param nimi käden nimi 
+     * @param nimi Käden nimi 
      */
     public Kasi(String nimi) {
         this.nimi = nimi;
     }
+
+    /**
+     * Lisää ensimmäisen tämän käden voittavan käden
+     * 
+     * @param k Voittava käsi 
+     */
+    public void lisaaVoittavaKasiYksi(Kasi k) {
+        this.tamaVoittaaYksi = k;
+    }
     
     /**
-     * Palauttaa käden nimen
+     * Lisää toisen tämän käden voittavan käden
      * 
-     * @return nimi 
+     * @param k Voitttava käsi
+     */
+    public void lisaaVoittavaKasiKaksi(Kasi k) {
+        this.tamaVoittaaKaksi = k;
+    }
+    /**
+     * Palauttaa käden nimen
+     *
+     * @return nimi
      */
     public String getNimi() {
         return this.nimi;
+    }
+    
+    /**
+     * Kertoo voittaako tämä käsi argumenttina annetun käden.
+     * Mikäli kädet ovat samat on tulos false.
+     * 
+     * @param voitettava käsi johon tätä kättä verrataan
+     * @return true jos tämä käsi voittaa, muuten false
+     */
+    public boolean voittaako(Kasi voitettava) {
+        if (this.tamaVoittaaYksi != null) {
+            if (this.tamaVoittaaYksi.getNimi().equals(voitettava.getNimi())) {
+                return false;
+            }
+        } 
+        if (this.tamaVoittaaKaksi != null) {
+            if (this.tamaVoittaaKaksi.getNimi().equals(voitettava.getNimi())) {
+                return false;
+            }
+        }
+        if (this.nimi.equals(voitettava.getNimi())) {
+            return false;
+        }
+        return true;
     }
 }
--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
@@ -1,12 +1,23 @@
 package com.mycompany.tiralabra_maven;
 
-import com.mycompany.ui.Cli;
+import com.mycompany.logiikka.Kasi;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Cli c = new Cli(1);
-        c.run();
+//        Cli c = new Cli(1);
+//        c.run();
+        Kasi kivi = new Kasi("KIVI");
+        Kasi paperi = new Kasi("PAPERI");
+        Kasi sakset = new Kasi("SAKSET");
+        
+        kivi.lisaaVoittavaKasiYksi(paperi);
+        paperi.lisaaVoittavaKasiYksi(sakset);
+        sakset.lisaaVoittavaKasiYksi(kivi);
+        
+        System.out.println("kivi vs paperi: " + kivi.voittaako(paperi));
+        System.out.println("kivi vs sakset: " + kivi.voittaako(sakset));
+        System.out.println("kivi vs kivi: " + kivi.voittaako(kivi));
     }
 }
