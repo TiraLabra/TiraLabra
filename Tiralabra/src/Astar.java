83deff62090ab3e6eda5a8623d8cfe142c247ebe
manuligit <riina.vaisanen@gmail.com>
2014-11-08 20:10:32 +0200

    Added Astar and Node classes

---
+++ Tiralabra/src/Astar.java
@@ -0,0 +1,153 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Astar {
+    public static void main(String[] args) {
+
+        //current map for testing purposes:
+        //  o______ start: 0,0
+        //  _______ end: 3,3
+        //  __x____ maxX: 5
+        //  _______ maxY: 5
+        //  _______
+
+        //TODO: set walls
+        //TODO: node creation based on map
+        //analyze map character by character, creating nodes and add !iswalkable is character is not _
+        //also set start & end according to map
+
+        Node start = new Node(0,0);
+        Node end = new Node(4,1);
+        Node current = start;
+        Node neighbor;
+
+        int maxX = 5;
+        int maxY = 5;
+        //list of unchecked nodes
+        //List open = new ArrayList();
+        PriorityQueue<Node> open = new PriorityQueue();
+
+        open.add(start);
+
+        //cost of the movement so far
+        double cost = 0;
+        cost = calculateHeuristic(end, current);
+
+
+        //list of nodes that have been checked
+        List closed = new ArrayList();
+
+        while (!(open.contains(end))) {
+            //current = remove lowest rank item from OPEN
+            current = open.poll();
+            closed.add(current);
+
+
+            //find neighbors of the start node which are walkable
+            //find out all possible directions of the current node
+            //remove walls + unwalkable squares + create a list of them
+
+
+
+
+
+            //for neighbours of the current:
+            //set node which we are going through now as neighbor
+            //F = G+H
+            //cost = g(current) + movementcost(current, neighbor)
+            //terrain cost is 1 because no terrain difference
+            cost = 1 + calculateHeuristic(end, neighbor);
+
+            //    if neighbor in OPEN and cost less than g(neighbor):
+            if (open.contains(neighbor) && cost<1) {
+                //remove neighbor from OPEN, because new path is better
+                open.remove(neighbor);
+            }
+
+            //if neighbor in CLOSED and cost less than g(neighbor): **
+            if (closed.contains(neighbor) && cost<1 ) {
+                //remove neighbor from CLOSED
+                closed.remove(neighbor);
+            }
+
+            //if neighbor not in OPEN and neighbor not in CLOSED:
+            if (!(open.contains(neighbor)) && !(closed.contains(neighbor))) {
+                //set g(neighbor) to cost
+                cost = 1;
+                //add neighbor to OPEN
+                open.add(neighbor);
+                //set priority queue rank to g(neighbor) + h(neighbor)
+
+                //set neighbor's parent to current
+                neighbor.setParent(current);
+
+            }
+
+        }
+
+        end.setParent(current);
+
+
+        //reconstruct reverse path from goal to start
+        //by following parent pointers
+
+        ArrayList path = new ArrayList();
+        Node help = new Node;
+
+        while (current.getParent() != null) {
+            path.add(current);
+            help = current.getParent();
+            current = help;
+        }
+
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i));
+        }
+        //add neighbor squares to open list
+        //save each node's parent
+        //find the neighbor which has the lowest F value (cost = g(current) + movementcost(current, neighbor))
+        //remove parent node from open list and add it to closed
+
+        
+    }
+
+
+    /**
+     * Checks, if the node is within the boundaries of map
+     * @param node The Node being checked
+     * @param maxX Maximum width of the map
+     * @param maxY Maximum height of the map
+     * @return True, if Node is within the boundaries of map
+     */
+    public boolean isWalkable(Node node, int maxX, int maxY) {
+        if (node.getX() >= 0 && node.getX() <= maxX) {
+            if (node.getY() >= 0 && node.getY() <= maxY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Creates the heuristic used for this A* algorithm, current heuristic for testing purposes is Manhattan distance.
+     * @param end The end Node at the map
+     * @param current Current Node where the algorithm is
+     * @return returns cost calculated by distance
+     */
+    public static double calculateHeuristic(Node end, Node current) {
+        double targetX = end.getX();
+        double currentX = current.getX();
+        double targetY = end.getY();
+        double currentY = current.getY();
+
+        double dx = Math.abs(targetX - currentX);
+        double dy = Math.abs(targetY - currentY);
+        double heuristic;
+        heuristic = dx+dy;
+        return heuristic;
+    }
+
+    
+}
---
+++ Tiralabra/src/Node.java
@@ -0,0 +1,93 @@
+/**
+ * Node is a coordinate within the Map. Node can be a start node, end node, wall or path.
+ *
+ * @author Riina Väisänen
+ */
+
+public class Node {
+    /** Node's x coordinate */
+    private int x;
+    /** Node's y coordinate */
+    private int y;
+    /** A property to check if Node is path, end or start point */
+    private boolean walkable;
+    /** Contains info if the Node has a parent Node (ie previous step) */
+    private Node parent;
+
+
+    /**
+     * Create a new node
+     * @param x The x coordinate on the map
+     * @param y The y coordinate on the map
+     */
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Get the x coordinate of the node
+     * @return The x coordinate of the node
+     */
+    public int getX() {
+        return x;
+    }
+
+
+    /**
+     * Set the x coordinate of the node
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Get the y coordinate of the node
+     * @return The y coordinate of the node
+     */
+    public int getY() {
+        return y;
+    }
+
+
+    /**
+     * Set the y coordinate of the node
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Get the walkability of the node:
+     * if node is start, end or path walkable is true
+     * @return whether the node is walkable
+     */
+    public boolean isWalkable() {
+        return walkable;
+    }
+
+
+    /**
+     * Set if the node is walkable
+     */
+    public void setWalkable(boolean walkable) {
+        this.walkable = walkable;
+    }
+
+    /**
+     * Gets the parent node
+     * @return parent node
+     */
+    public Node getParent() {
+        return this.parent;
+    }
+
+    /**
+     * Sets the specified node as the parent node
+     */
+    public void setParent(Node node) {
+        this.parent = node;
+    }
+
+
+}
