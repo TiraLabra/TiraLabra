a444d8010d0e577ff1732ebcde8d54ea1eb24686
appD-eino <eino-oskari.kiiski@helsinki.fi>
2014-11-21 16:52:55 +0200

    Käsilista javadoc

--- Tiralabra_maven/src/main/java/com/mycompany/tira/KasiLista.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tira/KasiLista.java
@@ -1,19 +1,36 @@
 package com.mycompany.tira;
 
+/**
+ * Linkitetty lista käsipareista. Listalla on ennalta määrätty koko. Kun listan koko saavuttaa
+ * enimmäiskoon, lista poistaa alusta jäseniä ja lisää uusia loppuun
+ * 
+ */
 public class KasiLista {
     
     private ListaSolmu eka;
     private int listanKoko;
     private int maara;
-    
+
+    /**
+     * Konstruktori alustaa luokan muuttujat
+     * 
+     * @param koko listan enimmäiskoko 
+     */
     public KasiLista(int koko) {
         this.eka = null;
         this.listanKoko = koko;
         this.maara = 0;
     }
     
-    public void lisaaKasipari(Kasipari k) {
-        ListaSolmu lisattava = new ListaSolmu(k, null);
+    /**
+     * Lisää listaan käsiparin. Jos listan maksimikoko on saavutettu,
+     *  poistetaan listan vanhin (ensimmäinen) alkio ja lisätään loppuun 
+     * uusin lisättävä käsipari
+     * 
+     * @param lisattavaPari listaan lisättävä käsipari
+     */
+    public void lisaaKasipari(Kasipari lisattavaPari) {
+        ListaSolmu lisattava = new ListaSolmu(lisattavaPari, null);
         ListaSolmu viimeinen = this.eka;
         
         if (this.maara == 0) {
@@ -33,6 +50,11 @@ public class KasiLista {
         }
     }
     
+    /**
+     * Palauttaa listan ensimmäisen jäsenen
+     * 
+     * @return listan ensimmäinen listasolmu
+     */
     public ListaSolmu getEnsimmainenSolmu() {
         return this.eka;
     }
