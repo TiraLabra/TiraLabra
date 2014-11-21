f4c5f152d4180804764f16f5192a13ec3c60a6fe
appD-eino <eino-oskari.kiiski@helsinki.fi>
2014-11-21 13:00:22 +0200

    Päävalikko tehty

--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
@@ -1,12 +1,27 @@
 package com.mycompany.tiralabra_maven;
 
 import com.mycompany.ui.Cli;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Cli c = new Cli(2);
-        c.run();
-     }
+public class App {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Pelimoodit:");
+            System.out.println("1. Kivi-Paperi-Sakset");
+            System.out.println("2. Kivi-Paperi-Sakset-Lisko-Spock");
+            System.out.print("> ");
+            Scanner lukija = new Scanner(System.in);
+            String komento = lukija.nextLine();
+            if (komento.equals("1")) {
+                Cli c = new Cli(1);
+                c.run();
+                break;
+            } else if (komento.equals("2")) {
+                Cli c = new Cli(2);
+                c.run();
+                break;
+            }
+        }
+    }
 }
