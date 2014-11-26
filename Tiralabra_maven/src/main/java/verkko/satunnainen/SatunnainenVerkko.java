fcb03e7c11e7dd213df2fcfdf529b241e7ffc987
saleer <eero.t.salminen>
2014-11-26 11:29:11 +0200

    SatunnainenVerkko
    Toinen verkko, jossa voidaan kokeilla hakuja

---
+++ Tiralabra_maven/src/main/java/verkko/satunnainen/SatunnainenVerkko.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verkko.satunnainen;
+
+import java.util.Comparator;
+import tira.DynaaminenLista;
+import tira.Lista;
+import verkko.rajapinnat.*;
+
+/**
+ * Verkko: nxn-taulukko, jossa voi edetä koordinaattiakselien suuntaan yhden ruudun kerrallaan
+ * 
+ * @author E
+ */
+public class SatunnainenVerkko implements Graph {
+    
+    private Value[][] verkko;
+    private Edge[][][] kaaret;
+    private double[][] painot;
+    
+    /**
+     * Luo verkon, jonka koko on nxn
+     * 
+     * @param koko 
+     */
+    public SatunnainenVerkko( int koko ) {
+        painot=new double[koko][koko];
+        verkko=new V[koko][koko];
+        kaaret=new E[koko][koko][1];        
+    }
+    
+    private void generoiSatunnainen() {
+        
+    }
+    ///////////////////////
+    /// GRAPH-RAJAPINTA ///
+    ///////////////////////
+    
+    /**
+     * 
+     * @param alku
+     * @param loppu
+     * @return 
+     */
+    public Iterable<Edge> getKaaret(Value alku, Value loppu) {
+        V l = (V) loppu;
+        Lista<Edge> returnvalue= new DynaaminenLista(kaaret[l.getX()][l.getY()].length);
+        for ( Edge e  : kaaret[l.getX()][l.getY()] ) 
+            returnvalue.add(e);
+        return returnvalue;
+    }
+    /**
+     * 
+     * @param alku
+     * @return 
+     */
+    public Iterable<Value> getNaapurit(Value alku) {
+        V l = (V) alku;
+        int x = l.getX(); int y = l.getY();
+        Lista<Value> returnvalue= new DynaaminenLista(4);
+        if ( x > 0 ) {
+            returnvalue.add(verkko[x-1][y]);
+        }
+        if ( x < verkko.length-1 ) {
+            returnvalue.add(verkko[x+1][y]);
+        }
+        if ( y > 0 ) {
+            returnvalue.add(verkko[x][y-1]);
+        }
+        if ( y < verkko.length-1 ) {
+            returnvalue.add(verkko[x][y+1]);
+        }        
+        return returnvalue;
+    }
+    
+}
+/**
+ * Verkon kaari
+ * 
+ * @author E
+ */
+class E implements Edge {
+    private double kustannus;
+
+    public E(double kustannus) {
+        this.kustannus = kustannus;
+    }
+    
+    public void setKustannus(double kustannus) {
+        this.kustannus = kustannus;
+    }
+    
+    public double getKustannus() {
+        return kustannus;
+    }
+    
+}
+
+/**
+ * Verkon solmu
+ * 
+ * @author E
+ */
+class V implements Value {
+    private int x,y;
+    
+    public int getX() {
+        return x;
+    }
+
+    public V(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    public double etaisyys(Value s) {
+        V v = (V) s;
+        return Math.abs( v.getX() - this.getX() )+Math.abs( v.getY() - this.getY() );
+    }
+    
+}
+/**
+ * Verkossa kuljettu polku
+ * 
+ * @author E
+ */
+class Polku implements Node {
+    
+    private double arvioituKustannus, kustannus;
+    private Value solmu;
+    private Edge  kuljettuKaari;
+    private Polku previous;
+
+    public double getArvioituKustannus() {
+        return arvioituKustannus;
+    }
+
+    public void setArvioituKustannus(double arvioituKustannus) {
+        this.arvioituKustannus = arvioituKustannus;
+    }
+
+    public double getKustannus() {
+        return kustannus;
+    }
+
+    public void setKustannus(double kustannus) {
+        this.kustannus = kustannus;
+    }
+
+    public Value getSolmu() {
+        return solmu;
+    }
+
+    public void setSolmu(Value solmu) {
+        this.solmu = solmu;
+    }
+
+    public Edge getKuljettuKaari() {
+        return kuljettuKaari;
+    }
+
+    public void setKuljettuKaari(Edge kuljettuKaari) {
+        this.kuljettuKaari = kuljettuKaari;
+    }
+
+    public Polku getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = (Polku)previous;
+    }
+
+    /**
+     * Node-rajapinnan toteutus.
+     * 
+     * @param x Vertailijan tarkkuus
+     * @return 
+     */
+    public Comparator<Node> vertailija( int x ) {
+        final int tarkkuus = x;
+        return new Comparator<Node>() {
+
+            public int compare(Node r1, Node r2) {
+                Polku a=(Polku)r1, b=(Polku)r2;
+                return (int) ( tarkkuus*( a.getArvioituKustannus() + a.getKustannus() - b.getArvioituKustannus() - b.getKustannus()));
+            }
+        };
+    }
+    
+}
