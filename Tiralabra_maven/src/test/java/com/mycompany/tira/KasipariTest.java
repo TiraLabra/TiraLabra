86f53f149f05fefbeda6b25006bed5c6d48a07e6
appD-eino <eino-oskari.kiiski@helsinki.fi>
2014-12-06 20:35:48 +0200

    KasipariTest v2

--- Tiralabra_maven/src/test/java/com/mycompany/tira/KasipariTest.java
+++ Tiralabra_maven/src/test/java/com/mycompany/tira/KasipariTest.java
@@ -8,41 +8,37 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class KasipariTest {
-    
+
     private Kasipari k;
-    
+
     public KasipariTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-//        k = new Kasipari(1,3,1);
+        k = new Kasipari(1, 3);
     }
-    
+
     @After
     public void tearDown() {
     }
 
-//    @Test
-//    public void getPelaajanKasiToimii() {
-//        assertEquals(1, k.getPelaajanKasi());
-//    }
-//    
-//    @Test
-//    public void getKoneenKasiToimii() {
-//        assertEquals(3, k.getKoneenKasi());
-//    }
-//    
-//    @Test
-//    public void getVoittajaToimii() {
-//        assertEquals(1, k.getVoittaja());
-//    }
+    @Test
+    public void pelaajanKasiOikein() {
+        assertEquals(1, this.k.getPelaajanKasi());
+    }
+    
+    @Test
+    public void koneenKasiOikein() {
+        assertEquals(3, this.k.getKoneenKasi());
+    }
+
 }
