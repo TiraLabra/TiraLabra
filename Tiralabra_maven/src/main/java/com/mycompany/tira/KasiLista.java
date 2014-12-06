a0edbaf7f6566ac05e8a8d52392ea8454dcea589
appD-eino <eino-oskari.kiiski@helsinki.fi>
2014-12-06 15:58:41 +0200

    Kasilista v2

--- Tiralabra_maven/src/main/java/com/mycompany/tira/KasiLista.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tira/KasiLista.java
@@ -1,61 +1,68 @@
 package com.mycompany.tira;
 
 /**
- * Linkitetty lista käsipareista. Listalla on ennalta määrätty koko. Kun listan koko saavuttaa
- * enimmäiskoon, lista poistaa alusta jäseniä ja lisää uusia loppuun
- * 
+ * Linkitetty lista käsipareista. Listalla on ennalta määrätty koko. Kun listan
+ * koko saavuttaa enimmäiskoon, vanhin listan jäsen poistetaan ja uusin lisätään
+ * listan alkuun. Toimintaperiaate on sama kuin pinolla
  */
 public class KasiLista {
-    
-    private ListaSolmu eka;
+
+    private Listasolmu eka;
     private int listanKoko;
     private int maara;
 
     /**
      * Konstruktori alustaa luokan muuttujat
-     * 
-     * @param koko listan enimmäiskoko 
+     *
+     * @param koko listan enimmäiskoko
      */
     public KasiLista(int koko) {
         this.eka = null;
-        this.listanKoko = koko;
         this.maara = 0;
+        this.listanKoko = koko;
+    }
+
+    /**
+     * Palauttaa viimeksi lisätyn käsiparin
+     * 
+     * @return viimeisin käsipari
+     */
+    public Kasipari getViimeisinPari() {
+        return this.eka.getKasipari();
+    }
+
+    /**
+     * Palauttaa listan ensimmäisen solmun
+     * 
+     * @return ensimmäinen listasolmu
+     */
+    public Listasolmu getEkaSolmu() {
+        return this.eka;
     }
     
     /**
-     * Lisää listaan käsiparin. Jos listan maksimikoko on saavutettu,
-     *  poistetaan listan vanhin (ensimmäinen) alkio ja lisätään loppuun 
-     * uusin lisättävä käsipari
-     * 
+     * Lisää listaan käsiparin. Jos listan maksimikoko on saavutettu, poistetaan
+     * listan vanhin alkio ja lisätään sitten uusi käsipari
+     *
      * @param lisattavaPari listaan lisättävä käsipari
      */
     public void lisaaKasipari(Kasipari lisattavaPari) {
-        ListaSolmu lisattava = new ListaSolmu(lisattavaPari, null);
-        ListaSolmu viimeinen = this.eka;
-        
+        Listasolmu lisattava = new Listasolmu(lisattavaPari, null);
+        Listasolmu viimeinen = this.eka;
+
         if (this.maara == 0) {
             this.eka = lisattava;
-        } else if (this.maara <= this.listanKoko) {
-            for (int i=0; i<this.maara; i++) {
-                viimeinen = viimeinen.getSeuraavaListaSolmu();
-            }
-            viimeinen.setSeuraavaListaSolmu(lisattava);
             this.maara++;
         } else {
-            this.eka = this.eka.getSeuraavaListaSolmu();
-            for (int i=0; i<this.listanKoko; i++) {
+            lisattava.setSeuraavaListaSolmu(this.eka);
+            this.eka = lisattava;
+            this.maara++;
+        }
+        if (this.maara > this.listanKoko) {
+            for (int i = 0; i < this.listanKoko-1; i++) {
                 viimeinen = viimeinen.getSeuraavaListaSolmu();
             }
-            viimeinen.setSeuraavaListaSolmu(lisattava);
+            viimeinen.setSeuraavaListaSolmu(null);
         }
-    }
-    
-    /**
-     * Palauttaa listan ensimmäisen jäsenen
-     * 
-     * @return listan ensimmäinen listasolmu
-     */
-    public ListaSolmu getEnsimmainenSolmu() {
-        return this.eka;
     }
 }
