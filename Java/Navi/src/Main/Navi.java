83e020eb3cbe039edefa80ed732d77efccc0f7a7
Leevi Heino <leeviheino@live.fi>
2015-09-14 00:21:37 +0300

    Karttalogiikkaa

--- Java/Navi/src/Main/Navi.java
+++ Java/Navi/src/Main/Navi.java
@@ -1,9 +1,13 @@
 package Main;
+import Terrain.CartesianMap;
 
 // @author Leevi
 public class Navi {
 
     public static void main(String[] args) {
-
+        
+        CartesianMap map = new CartesianMap(28, 20);
+        map.displayMap();
+        
     }
 }
--- Java/Navi/src/Terrain/CartesianMap.java
+++ Java/Navi/src/Terrain/CartesianMap.java
@@ -2,5 +2,40 @@ package Terrain;
 
 // @author Leevi
 public class CartesianMap {
-    // TODO: Implementation
+
+    int x;
+    int y;
+    int [ ] [ ] map;
+
+    public CartesianMap(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+        this.map = new int  [x] [y];
+
+    }
+
+    public void generateTerrain(boolean hasRoughTerrain) {
+
+        // TODO: Implementation
+    }
+
+    public int getSingleTile(int xPos, int yPos) {
+
+        // TODO: Implementation
+        return -1;
+
+    }
+
+    public void displayMap() {
+
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+
+    }
+
 }
