39da0d2d691fde5d57bdc66e3847703d2d9fb938
Leevi Heino <leeviheino@live.fi>
2015-09-29 14:37:05 +0300

    UI-parannuksia

--- Java/Navi/src/UI/TestUserInterface.java
+++ Java/Navi/src/UI/TestUserInterface.java
@@ -29,10 +29,10 @@ public class TestUserInterface {
         int goalY = 0;
         
         System.out.println("Select terrain");
-        System.out.println("--Flat, type '1'");
-        System.out.println("--Rough, type '2'");
-        System.out.println("--Preset 1, type '3'");
-        System.out.print(" > ");
+        System.out.println("  Flat, type '1'");
+        System.out.println("  Rough, type '2'");
+        System.out.println("  Preset 1, type '3'");
+        System.out.print("> ");
         
         Scanner scanner = new Scanner(System.in);
         
@@ -47,51 +47,51 @@ public class TestUserInterface {
             else if (terrain == 3) {
                 break;
             }
-            System.out.print(" > ");
+            System.out.print("> ");
         }
         
         System.out.println("Set startX, type [0-" + (Navi.xLim - 1) + "]");
-        System.out.print(" > ");
+        System.out.print("> ");
         
         while (true) {
             startX = scanner.nextInt();
             if (startX > -1 && startX < Navi.xLim) {
                 break;
             }
-            System.out.print(" > ");
+            System.out.print("> ");
         }
         
         System.out.println("Set startY, type [0-" + (Navi.yLim - 1) + "]");
-        System.out.print(" > ");
+        System.out.print("> ");
         
         while (true) {
             startY = scanner.nextInt();
             if (startY > -1 && startY < Navi.yLim) {
                 break;
             }
-            System.out.print(" > ");
+            System.out.print("> ");
         }
         
         System.out.println("Set goalX, type [0-" + (Navi.xLim - 1) + "]");
-        System.out.print(" > ");
+        System.out.print("> ");
         
         while (true) {
             goalX = scanner.nextInt();
             if (goalX > -1 && goalX < Navi.xLim) {
                 break;
             }
-            System.out.print(" > ");
+            System.out.print("> ");
         }
         
         System.out.println("Set goalY, type [0-" + (Navi.yLim - 1) + "]");
-        System.out.print(" > ");
+        System.out.print("> ");
         
         while (true) {
             goalY = scanner.nextInt();
             if (goalY > -1 && goalY < Navi.yLim) {
                 break;
             }
-            System.out.print(" > ");
+            System.out.print("> ");
         }
         
         run(terrain, startX, startY, goalX, goalY);
@@ -114,6 +114,22 @@ public class TestUserInterface {
         
         Pathfinder finder = new Pathfinder();
         int[][] route = finder.determineRoute(Navi.xLim, Navi.yLim, map.getMap(), startX, startY, goalX, goalY);
+        
+        System.out.println("\nMap:\n");
+        map.displayMap();
+        
+        System.out.println("\nRoute:\n");
+        map.displayMapWithRoute(route);
+        
+        System.out.println("\nLegend:\n");
+        System.out.println("  '0' = Void");
+        System.out.println("  '*' = Dirtroad");
+        System.out.println("  '/' = Traffic");
+        System.out.println("  '-' = Road");
+        System.out.println("  '=' = Highway");
+        System.out.println("  'X' = Route node");
+        System.out.println("  'S' = Start");
+        System.out.println("  'G' = Goal");
         
     }
     
