f3229d94750c7d9ea7c8141aeb5406e13046c28f
Leevi Heino <leeviheino@live.fi>
2015-09-30 12:18:51 +0300

    Javadoccii

--- Java/Navi/src/Terrain/CartesianMap.java
+++ Java/Navi/src/Terrain/CartesianMap.java
@@ -3,21 +3,29 @@ package Terrain;
 import java.util.Random;
 import CoreLogic.AStarPathfinder;
 
-// @author Leevi
+/**
+ *
+ * @author Leevi
+ */
 public class CartesianMap {
-    
+
     //================================================================================
     // Parameters
     //================================================================================
-
+    
     private final int xLim;
     private final int yLim;
     private int[][] map;
-    
+
     //================================================================================
     // Constructors
     //================================================================================
-
+    
+    /**
+     *
+     * @param x
+     * @param y
+     */
     public CartesianMap(int x, int y) {
 
         this.xLim = x;
@@ -25,11 +33,15 @@ public class CartesianMap {
         this.map = new int[x][y];
 
     }
-    
+
     //================================================================================
     // Map generation
     //================================================================================
-
+    
+    /**
+     *
+     * @param hasRoughTerrain
+     */
     public void generateTerrain(boolean hasRoughTerrain) {
 
         for (int y = 0; y < yLim; y++) {
@@ -38,24 +50,19 @@ public class CartesianMap {
                 int tileValue;
                 if (!hasRoughTerrain) {
                     tileValue = rand.nextInt(38 + 1);
-                }
-                else {
+                } else {
                     tileValue = rand.nextInt(18 + 1);
                 }
-                
+
                 if (tileValue < 3) {
                     tileValue = -1; // Void
-                }
-                else if (tileValue < 7) {
+                } else if (tileValue < 7) {
                     tileValue = 0; // Dirtroad
-                }
-                else if (tileValue < 10) {
+                } else if (tileValue < 10) {
                     tileValue = 1; // Traffic
-                }
-                else if (tileValue < 18) {
+                } else if (tileValue < 18) {
                     tileValue = 2; // Road
-                }
-                else {
+                } else {
                     tileValue = 3; // Highway
                 }
                 map[x][y] = tileValue;
@@ -63,84 +70,89 @@ public class CartesianMap {
         }
 
     }
-    
+
+    /**
+     *
+     */
     public void generateTerrainPreset1() {
-        
+
         for (int y = 0; y < yLim; y++) {
             for (int x = 0; x < xLim; x++) {
                 Random rand = new Random();
                 int tileValue;
                 tileValue = rand.nextInt(30 + 1);
-                
+
                 if (x > 2 && x < xLim - 5 && y == 5) {
                     tileValue = -1;
-                }
-                else if (y > 5 && y < yLim - 8 && x == 3) {
+                } else if (y > 5 && y < yLim - 8 && x == 3) {
                     tileValue = -1;
-                }
-                else if (y > 5 && y < yLim - 8 && x == 15) {
+                } else if (y > 5 && y < yLim - 8 && x == 15) {
                     tileValue = -1;
-                }
-                else {
+                } else {
                     if (tileValue < 3) {
                         tileValue = -1; // Void
-                    }
-                    else if (tileValue < 7) {
+                    } else if (tileValue < 7) {
                         tileValue = 0; // Dirtroad
-                    }
-                    else if (tileValue < 10) {
+                    } else if (tileValue < 10) {
                         tileValue = 1; // Traffic
-                    }
-                    else if (tileValue < 18) {
+                    } else if (tileValue < 18) {
                         tileValue = 2; // Road
-                    }
-                    else {
+                    } else {
                         tileValue = 3; // Highway
                     }
                 }
                 map[x][y] = tileValue;
             }
         }
-        
+
     }
-    
+
     //================================================================================
     // Getters
     //================================================================================
-
+    
+    /**
+     *
+     * @param xPos
+     * @param yPos
+     * @return
+     */
     public int getSingleTile(int xPos, int yPos) {
 
         return map[xPos][yPos];
 
     }
-    
+
+    /**
+     *
+     * @return
+     */
     public int[][] getMap() {
-        
+
         return map;
-        
+
     }
-    
+
     //================================================================================
     // Map drawing
     //================================================================================
-
+    
+    /**
+     *
+     */
     public void displayMap() {
 
         for (int y = 0; y < yLim; y++) {
             for (int x = 0; x < xLim; x++) {
                 if (getSingleTile(x, y) < 0) {
                     System.out.print(" 0 "); // Void
-                }
-                else if (getSingleTile(x, y) == 0) { // Dirtroad
+                } else if (getSingleTile(x, y) == 0) { // Dirtroad
                     System.out.print(" * ");
-                }
-                else if (getSingleTile(x, y) == 1) { // Traffic
+                } else if (getSingleTile(x, y) == 1) { // Traffic
                     System.out.print(" / ");
-                }
-                else if (getSingleTile(x, y) == 2) { // Road
+                } else if (getSingleTile(x, y) == 2) { // Road
                     System.out.print(" - ");
-                }
-                else if (getSingleTile(x, y) == 3) { // Highway
+                } else if (getSingleTile(x, y) == 3) { // Highway
                     System.out.print(" = ");
                 }
             }
@@ -148,11 +160,14 @@ public class CartesianMap {
         }
 
     }
-    
-    public void displayMapWithRoute(int [][] route) {
-        
+
+    /**
+     *
+     * @param route
+     */
+    public void displayMapWithRoute(int[][] route) {
+
         // TODO: Implementation
-        
     }
 
 }
